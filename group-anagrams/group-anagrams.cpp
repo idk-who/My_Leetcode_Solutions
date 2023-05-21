cdb88dcabc45250a19e6437e82953e0878add069
idk-who <idontknowwhodoi@gmail.com>
2023-05-22 00:51:51 +0530

    Time: 31 ms (90.45%) | Memory: 19.5 MB (88.62%) - LeetSync

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> dict;
         string hash;
-        for (auto& s : strs){
-            hash = s;
+        for (int i=0; i<strs.size(); i++){
+            hash = strs[i];
             sort(hash.begin(), hash.end());
-            dict[hash].push_back(s);  
+            dict[hash].push_back(strs[i]);  
         }
 
         vector<vector<string>> answer;
