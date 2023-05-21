ababb2e2b1e54297bf851a34ad65b998c1f60276
idk-who <idontknowwhodoi@gmail.com>
2023-05-22 00:52:21 +0530

    Update group-anagrams_map-sort.cpp

--- group-anagrams/group-anagrams_map-sort.cpp
+++ group-anagrams/group-anagrams_map-sort.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> dict;
         string hash;
-        for (int i=0; i<strs.size(); i++){
-            hash = strs[i];
+        for (auto& s : strs){
+            hash = s;
             sort(hash.begin(), hash.end());
-            dict[hash].push_back(strs[i]);  
+            dict[hash].push_back(s);  
         }
 
         vector<vector<string>> answer;
 
-        for (auto i : dict){
+        for (auto& i : dict){
             answer.push_back(i.second);
         }
         
