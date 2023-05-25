f8c9c986da10bd00b6b7b74e4c25dc8bafe36c3b
idk-who <idontknowwhodoi@gmail.com>
2023-05-25 21:37:44 +0530

    Time: 61 ms (44.11%) | Memory: 17.7 MB (21.48%) - LeetSync

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(a.lower() for a in s if a.isalnum())
+        s = [a for a in s.lower() if a.isalnum()]
         return s == s[::-1]
