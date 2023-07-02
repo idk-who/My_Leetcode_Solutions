8644a3f69ac77a47186b8396311a2f9c63a52757
idk-who <idontknowwhodoi@gmail.com>
2023-07-02 23:51:49 +0530

    Time: 1568 ms (53.46%) | Memory: 6.9 MB (62.11%) - LeetSync

---
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+public:
+    int unfairness = INT_MAX;
+    vector<int> dsitribution;
+
+    int distributeCookies(vector<int> &cookies, int k)
+    {
+        dsitribution = vector<int>(k, 0);
+        distributeCookiesRec(cookies, k);
+        return unfairness;
+    }
+
+    void distributeCookiesRec(vector<int> &cookies, int k, int counter = 0)
+    {
+        if (counter == cookies.size())
+        {
+            calculate_unfairness();
+            return;
+        }
+
+        for (int j = 0; j < k; j++)
+            {
+                dsitribution[j] += cookies[counter];
+                distributeCookiesRec(cookies, k, counter + 1);
+                dsitribution[j] -= cookies[counter];
+            }
+        return;
+    }
+
+    void calculate_unfairness()
+    {
+        int max = INT_MIN;
+        for (auto &i : dsitribution)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        if (max < unfairness)
+        {
+            unfairness = max;
+        }
+    }
+};
