c8ef0d5b2fd5254e0767b2961f44daa60d23ac20
idk-who <idontknowwhodoi@gmail.com>
2023-07-03 14:10:35 +0530

    Time: 3 ms (78.34%) | Memory: 7 MB (19.51%) - LeetSync

---
+++ buddy-strings/buddy-strings.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    bool buddyStrings(string s, string goal)
+    {
+        if (s.size() != goal.size())
+            return false;
+
+        vector<vector<char>> different;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != goal[i])
+                different.push_back({s[i], goal[i]});
+        }
+
+        if (different.size() == 2 && different[0][0] == different[1][1] && different[0][1] == different[1][0])
+            return true;
+        else if (different.size() == 0)
+        {
+            vector<int> freq(26, 0);
+            for (int i = 0; i < s.size(); i++)
+            {
+                freq[s[i] - 'a']++;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                if (freq[i] > 1)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
