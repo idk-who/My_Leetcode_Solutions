8342cca7c46b956a18f16802ba8b1e523f33add7
idk-who <idontknowwhodoi@gmail.com>
2023-08-07 14:08:35 +0530

    Time: 55 ms (72.89%) | Memory: 16.9 MB (28.37%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    m = 0
+    n = 0
+    def searchMatrix(self, matrix, target: int) -> bool:
+        low = 0
+        m = len(matrix)
+        n = len(matrix[0])
+        high = m*n - 1
+
+        while(low<=high):
+            mid = (high-low) // 2 + low
+            index_ele = matrix[mid//n][mid%n]
+            if index_ele == target:
+                return True
+            elif index_ele < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return False
