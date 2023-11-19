3296b2a20989fb642bd6c8ee2c857585eda81e92
idk-who <idontknowwhodoi@gmail.com>
2023-11-20 00:42:58 +0530

    Time: 162 ms (13.95%) | Memory: 17.8 MB (73.64%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = defaultdict(lambda: 0)
+        dic = defaultdict(int)
         for i in nums:
             dic[i] += 1
         for i,j in dic.items():
