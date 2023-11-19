0fa8494b587a12d89b3545857dc841697eba99e3
idk-who <idontknowwhodoi@gmail.com>
2023-11-20 00:42:47 +0530

    Time: 155 ms (36.60%) | Memory: 17.9 MB (33.79%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = {}
+        dic = defaultdict(lambda: 0)
         for i in nums:
-            dic[i] = dic.get(i,0) + 1
+            dic[i] += 1
         for i,j in dic.items():
             if j > len(nums)/2:
                 return i
