b307fc3094c61250123d09fe2a321dfde6490265
idk-who <idontknowwhodoi@gmail.com>
2023-11-22 19:24:48 +0530

    Time: 70 ms (24.08%) | Memory: 17.8 MB (30.11%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        2 pointer approach
-
-        Algo:
-
-        i = buy date
-        j = sell date
-
-        i, j = 0
-        iterate till j<len(array)
-            profit = j-i
-            if profit<0:
-                j++
-            else: 
-                total_profit+=profit
-                j++
-            i=j
-        
-        """
-
         i = 0 
         j = 0
         total_profit = 0
