6ee3fc494cd665987e800a116b827e4318c20d31
idk-who <idontknowwhodoi@gmail.com>
2023-11-22 23:00:27 +0530

    Time: 432 ms (32.44%) | Memory: 17.7 MB (14.17%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,13 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        zero_indexes = [idx for idx, value in enumerate(nums[:-1]) if value == 0]
-        
-        for zero_ind in zero_indexes:
-            for i in range(zero_ind):
-                zero_ind-=1
-                if nums[zero_ind]>i+1:
-                    break   
-            else:
-                return False
-                
-        return True
+        reach = nums[0]
+        ptr = 0
+        while ptr<len(nums) and ptr<=reach:
+            reach = max(reach, ptr+nums[ptr])
+            if reach>=len(nums)-1:
+                return True
+            ptr+=1
+        return False
