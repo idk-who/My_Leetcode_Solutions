f68615622262ca38b33918782895df63a538a742
idk-who <idontknowwhodoi@gmail.com>
2023-12-19 01:59:43 +0530

    Time: 167 ms (11.84%) | Memory: 19.6 MB (10.67%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,31 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        """
+         0 = greater than no one
+         1 = greater than left
+         2 = greater than right 
+         3 = greater than left and right 
+        """
+        diff = [0]*len(ratings)
+        for i in range(len(ratings)):
+            if i != len(ratings)-1 and ratings[i]>ratings[i+1]:
+                diff[i]+=2
+            if i != 0 and ratings[i]>ratings[i-1]:
+                diff[i]+=1
+
+        candies = [0]*len(ratings)
+        for i in range(len(ratings)):
+            if diff[i] == 0:
+                candies[i]=1
+        for i in range(len(ratings)):
+            if diff[i] == 1:
+                candies[i] = candies[i-1]+1
+        for i in range(len(ratings)-1, -1, -1):
+            if diff[i] == 2:
+                candies[i] = candies[i+1]+1
+        for i in range(len(ratings)):
+            if diff[i] == 3:
+                candies[i] = max(candies[i-1], candies[i+1]) + 1
+            
+        return sum(candies)
+
