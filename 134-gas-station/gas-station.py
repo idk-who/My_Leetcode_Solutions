235976cd00cb81b54700f4d117052e42fb34bd7c
idk-who <idontknowwhodoi@gmail.com>
2023-12-18 18:49:05 +0530

    Time: 1142 ms (10.30%) | Memory: 22 MB (73.04%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,10 +1,5 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        """
-        Okay, so my approach is to 
-        1) go back from end if step 
-        """
-
         tank = 0
         start = 0
         end  = 0
@@ -12,7 +7,6 @@ class Solution:
 
         while start<n:
             tank+=gas[end%n]
-            print(start, end, tank)
             if end%n == start and end>start:
                 return start
             elif tank>=cost[end%n]:
