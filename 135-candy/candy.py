77217f957869f18ed7821e4ce72a677d96d6fdd5
idk-who <idontknowwhodoi@gmail.com>
2023-12-21 21:20:22 +0530

    Time: 121 ms (99.79%) | Memory: 20.2 MB (7.85%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,9 +1,8 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        
         candies = len(ratings)
-        valley = 0
         ptr = 1
+
         while ptr<len(ratings): 
 
             peak = 0
@@ -11,14 +10,12 @@ class Solution:
                 peak += 1
                 candies += peak
                 ptr += 1
-                # print("incr", ptr, peak, valley, candies)
 
             valley = 0
             while ptr<len(ratings) and ratings[ptr] < ratings[ptr-1]:
                 valley += 1
                 candies += valley
                 ptr += 1
-                # print("decr", ptr, peak, valley, candies)
 
             candies -= min(peak, valley)
             
@@ -26,8 +23,5 @@ class Solution:
                 peak = ptr
                 valley = ptr
                 ptr += 1
-                # print("nutr", ptr, peak, valley, candies)
             
         return candies
-
-
