59b5cc3ee3722537f7cbf364e54428d70815dd83
idk-who <idontknowwhodoi@gmail.com>
2023-12-21 21:19:40 +0530

    Time: 128 ms (98.62%) | Memory: 21 MB (5.22%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,13 +1,33 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies = [1]*len(ratings)
+        
+        candies = len(ratings)
+        valley = 0
+        ptr = 1
+        while ptr<len(ratings): 
 
-        for i in range(1, len(ratings)):
-            if ratings[i]>ratings[i-1]:
-                candies[i]=candies[i-1]+1
-        for i in range(len(ratings)-2, -1, -1):
-            if ratings[i]>ratings[i+1]:
-                candies[i] = max(candies[i+1]+1, candies[i])
+            peak = 0
+            while ptr<len(ratings) and ratings[ptr] > ratings[ptr-1]:
+                peak += 1
+                candies += peak
+                ptr += 1
+                # print("incr", ptr, peak, valley, candies)
+
+            valley = 0
+            while ptr<len(ratings) and ratings[ptr] < ratings[ptr-1]:
+                valley += 1
+                candies += valley
+                ptr += 1
+                # print("decr", ptr, peak, valley, candies)
+
+            candies -= min(peak, valley)
             
-        return sum(candies)
+            while ptr<len(ratings) and ratings[ptr] == ratings[ptr-1]:
+                peak = ptr
+                valley = ptr
+                ptr += 1
+                # print("nutr", ptr, peak, valley, candies)
+            
+        return candies
+
 
