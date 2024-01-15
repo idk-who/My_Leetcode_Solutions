a7f6a84e7f7855617e0b96a35e182c7b010c34f5
idk-who <idontknowwhodoi@gmail.com>
2024-01-15 12:34:27 +0530

    Time: 40 ms (98.55%) | Memory: 17.4 MB (31.01%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -7,21 +7,16 @@ class Solution:
         for i in range(numRows):
             start = i 
             end  = len(s)
-            # print("\n\nLoop", i+1)
             while start<end:
-                # print(i+1, start, "diff1", diff1)
                 if diff1>0:
                     ans.append(s[start])
                     start+=diff1
-
+                if start>=end:
+                    break 
                 if diff2>0:
-                    if start>=end:
-                        break 
                     ans.append(s[start])
                     start+=diff2
             diff1 -= 2
             diff2 += 2
-
-        # print(''.join(ans))
 
         return ''.join(ans)
