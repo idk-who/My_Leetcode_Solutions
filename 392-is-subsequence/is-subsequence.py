a537803d2b1160e5cbb51de9f8380fc6dcf3cafd
idk-who <idontknowwhodoi@gmail.com>
2024-03-30 18:48:34 +0530

    Time: 40 ms (26.77%) | Memory: 16.6 MB (89.32%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -6,8 +6,6 @@ class Solution:
         while p1<len(s) and p2<len(t):
             if s[p1] == t[p2]:
                 p1 += 1
-                p2 += 1
-            else:
-                p2 += 1
-        
+            p2 += 1
+    
         return p1 == len(s)
