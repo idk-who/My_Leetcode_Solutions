da1291b4aefd234d2e02405a36038017d9ac5981
idk-who <idontknowwhodoi@gmail.com>
2024-04-07 21:31:46 +0530

    Time: 4110 ms (41.38%) | Memory: 16.8 MB (99.35%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,24 +1,22 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        word_dict = Counter(words)
+
         n = len(words)
         m = len(words[0])
         k = len(s)
         ans = []
-        word_dict = defaultdict(int)
-        for w in words:
-            word_dict[w] += 1
-        for i in range(k):
-            temp = word_dict.copy()
 
-            j = i
-            while j+m<=k and temp[s[j:j+m]]>0:
-                temp[s[j:j+m]] -= 1
-                j += m
-            
-            for v in temp.values():
-                if v > 0:
+        for i in range(k-n*m+1):
+            temp = defaultdict(int)
+
+            for j in range(i, i+n*m, m):
+                if s[j:j+m] in word_dict:
+                    temp[s[j:j+m]] += 1
+                else:
                     break
-            else:
+            
+            if temp == word_dict:
                 ans.append(i)
 
         return ans
