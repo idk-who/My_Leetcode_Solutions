fce61999f857ebaa861e37e9166d350a3ee867fc
idk-who <idontknowwhodoi@gmail.com>
2024-04-08 22:55:42 +0530

    Time: 32 ms (74.49%) | Memory: 16.5 MB (31.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,8 +4,6 @@ class Solution:
         return self.helper(n)
     def helper(self, n):
         if n == 0 or n == 1: return 1
-        if (n, 1) not in self.dp:
-            self.dp[(n, 1)] = self.helper(n-1)
-        if (n, 2) not in self.dp:
-            self.dp[(n, 2)] = self.helper(n-2)
-        return self.dp[(n, 1)] + self.dp[(n, 2)] 
+        if n not in self.dp:
+            self.dp[n] = self.helper(n-1) + self.helper(n-2)
+        return self.dp[n]
