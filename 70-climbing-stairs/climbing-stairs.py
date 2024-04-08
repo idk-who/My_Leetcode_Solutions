6e99bcb994482543fd8901b111096c1ecc067f0e
idk-who <idontknowwhodoi@gmail.com>
2024-04-08 22:58:15 +0530

    Time: 34 ms (59.32%) | Memory: 16.5 MB (31.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,9 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        self.dp = dict()
+        self.dp = [None]*(n+1)
         return self.helper(n)
     def helper(self, n):
-        if n == 0 or n == 1: return 1
-        if n not in self.dp:
+        if n < 2: return 1
+        if self.dp[n] is None:
             self.dp[n] = self.helper(n-1) + self.helper(n-2)
         return self.dp[n]
