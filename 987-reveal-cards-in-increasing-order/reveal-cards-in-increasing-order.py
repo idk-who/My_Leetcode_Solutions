985b3ffbe04e6614430aaf68b9c2924069522fa1
idk-who <idontknowwhodoi@gmail.com>
2024-04-10 23:50:40 +0530

    Time: 44 ms (68.69%) | Memory: 16.8 MB (80.95%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,10 +1,9 @@
-from collections import deque
-
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         deck.sort(reverse=True)
-
+        
         ans = deque()
+
         for i in deck:
             ans.rotate()
             ans.appendleft(i)
