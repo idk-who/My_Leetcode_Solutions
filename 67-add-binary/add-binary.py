01bceb61c73352763e14ab64a286e696185e5e93
idk-who <idontknowwhodoi@gmail.com>
2024-04-12 11:38:49 +0530

    Time: 41 ms (32.51%) | Memory: 16.7 MB (10.95%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,14 +1,21 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res = ""
-        i, j, carry = len(a) - 1, len(b) - 1, 0
-        while i >= 0 or j >= 0:
-            sum = carry;
-            if i >= 0 : sum += ord(a[i]) - ord('0') # ord is use to get value of ASCII character
-            if j >= 0 : sum += ord(b[j]) - ord('0')
-            i, j = i - 1, j - 1
-            carry = 1 if sum > 1 else 0;
-            res += str(sum % 2)
+        res = []
 
-        if carry != 0 : res += str(carry);
-        return res[::-1]
+        i = len(a)-1
+        j = len(b)-1
+        carry = 0
+        while i >= 0 or j >= 0:
+            sum = carry 
+            if i>=0 and a[i] == '1' : sum += 1
+            if j>=0 and b[j] == '1' : sum += 1
+
+            carry = 1 if sum > 1 else 0
+            res.append(str(sum%2))
+
+            i -= 1
+            j -= 1
+
+        if carry == 1: res.append("1")
+        
+        return "".join(res[::-1])
