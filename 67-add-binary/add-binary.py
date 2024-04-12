151bfa4678c33c5a9a3320d716e4af7c675adc9b
idk-who <idontknowwhodoi@gmail.com>
2024-04-12 11:37:59 +0530

    Time: 42 ms (26.53%) | Memory: 16.7 MB (10.95%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -10,12 +10,12 @@ class Solution:
             if i>=0 and a[i] == '1' : sum += 1
             if j>=0 and b[j] == '1' : sum += 1
 
-            carry = sum // 2
-            if sum%2 == 0: res.append('0') 
-            else: res.append('1')
+            carry = 1 if sum > 1 else 0
+            res.append(str(sum%2))
 
             i -= 1
             j -= 1
+
         if carry == 1: res.append("1")
         
         return "".join(res[::-1])
