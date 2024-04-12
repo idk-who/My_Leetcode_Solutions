446ac3fb1b6e38c28e4484cfbb4d1df14c1031ac
idk-who <idontknowwhodoi@gmail.com>
2024-04-12 11:35:46 +0530

    Time: 34 ms (77.99%) | Memory: 16.6 MB (35.36%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,3 +1,21 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2)+int(b, 2))[2:]
+        res = []
+
+        i = len(a)-1
+        j = len(b)-1
+        carry = 0
+        while i >= 0 or j >= 0:
+            sum = carry 
+            if i>=0 and a[i] == '1' : sum += 1
+            if j>=0 and b[j] == '1' : sum += 1
+
+            carry = sum // 2
+            if sum%2 == 0: res.append('0') 
+            else: res.append('1')
+
+            i -= 1
+            j -= 1
+        if carry == 1: res.append("1")
+        
+        return "".join(res[::-1])
