a23ef9eda19e086dd3dc48a7f4e5244bdcd3808a
idk-who <idontknowwhodoi@gmail.com>
2024-04-13 23:22:14 +0530

    Time: 45 ms (27.62%) | Memory: 16.5 MB (94.32%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -11,14 +11,10 @@ class Solution:
     def helper(self, ptr = 0):
         if ptr == len(self.s): return True
         if ptr > len(self.s): return False
-        # print("ptr:", ptr)
         if self.dp[ptr] is None:
             temp = []
             for i in range(ptr+self.minWL, ptr+self.maxWL+1):
-                # print("i", i)
-                # print("self.s[ptr: i]", self.s[ptr: i])
                 if self.s[ptr: i] in self.words:
-                    # print("ptr+i", ptr+i)
                     temp.append(self.helper(i))
             print(temp)
             self.dp[ptr] = any(temp)
