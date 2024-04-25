a1a072d3c9578bdf552114634395b2ba3af1e58e
idk-who <idontknowwhodoi@gmail.com>
2024-04-25 23:07:30 +0530

    Time: 1207 ms (54.08%) | Memory: 18.1 MB (20.00%) - LeetSync

---
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        freq = [-1]*26
+        s = map(lambda x:ord(x)-97, list(s))
+        for i in s:
+            m = 0
+            for f in range(i-k, i+k+1):
+                if f >= 0 and f < 26:
+                    m = max(m, freq[f])
+            freq[i] = m+1
+        return max(freq)
+            
