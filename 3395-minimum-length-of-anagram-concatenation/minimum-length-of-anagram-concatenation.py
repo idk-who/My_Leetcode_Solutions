a6e89fc40c60b17126e7f9290c6b23ccc62d6caa
idk-who <idontknowwhodoi@gmail.com>
2024-05-05 23:10:54 +0530

    Time: 1190 ms (100.00%) | Memory: 18.1 MB (100.00%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
@@ -1,29 +1,18 @@
-from collections import Counter
-
 class Solution:
     def minAnagramLength(self, s: str) -> int:
-        # Get the length of the input string 's'
+        stringList = list(s)
         n = len(s)
+        ans = n
+        for size in range(1, n):
+            if n % size == 0: 
+                count = Counter(stringList[:size])
 
-        # Define a helper function to calculate the greatest common divisor (GCD)
-        def gcd(a, b):
-            # Euclidean algorithm to find GCD
-            while b != 0:
-                temp = b
-                b = a % b
-                a = temp
-            return a
+                for j in range(size, n, size):
+                    temp = Counter(stringList[j:j+size])
 
-        # Count the occurrences of each character in 's' using Counter
-        hashmap = Counter(s)
-
-        # Initialize the divisor with the count of the first character in 's'
-        div = hashmap[s[0]]
-
-        # Iterate over the counts of characters in the hashmap
-        for key, value in hashmap.items():
-            # Update the divisor by finding the GCD of the current count and the previous divisor
-            div = gcd(div, value)
-        
-        # Return the minimum length of 't', which is the length of 's' divided by the divisor
-        return n // div
+                    if count != temp:
+                        break
+                else:
+                    return size
+                    
+        return ans
