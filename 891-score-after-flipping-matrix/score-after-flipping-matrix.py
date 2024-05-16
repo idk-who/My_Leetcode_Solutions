0ea78bc4d040e99ae9f081b0a8acbe8781613e72
idk-who <idontknowwhodoi@gmail.com>
2024-05-16 22:58:56 +0530

    Time: 36 ms (86.91%) | Memory: 16.5 MB (55.58%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,8 +1,21 @@
 class Solution:
-    def matrixScore(self, A):
-        M, N = len(A), len(A[0])
-        res = (1 << N - 1) * M
-        for j in range(1, N):
-            cur = sum(A[i][j] == A[i][0] for i in range(M))
-            res += max(cur, M - cur) * (1 << N - 1 - j)
-        return res
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        for row in range(len(grid)):
+            if not grid[row][0]:
+                for j in range(len(grid[0])):
+                    grid[row][j] = 0 if grid[row][j] else 1
+        
+        for col in range(len(grid[0])):
+            if sum(row[col] for row in grid) < len(grid)/2:
+                for i in range(len(grid)):
+                    grid[i][col] = 0 if grid[i][col] else 1
+
+        ans = 0
+        for row in grid:
+            temp = 0
+            for col in row:
+                temp <<= 1
+                temp |= col
+            ans += temp
+        
+        return ans
