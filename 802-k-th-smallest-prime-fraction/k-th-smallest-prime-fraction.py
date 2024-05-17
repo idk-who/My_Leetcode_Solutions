4f5f5a17783cdf56dafa7bb4dbcb734ef44caba3
idk-who <idontknowwhodoi@gmail.com>
2024-05-18 00:06:53 +0530

    Time: 53 ms (98.52%) | Memory: 16.8 MB (73.20%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,30 +1,33 @@
+import heapq
+
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         n = len(arr)
-        left, right = 0, 1
-        res = []
 
+        left = 0
+        right = 1
+        
         while left <= right:
-            mid = left + (right - left) / 2
+            mid = left + (right-left)/2
+
             j, total, num, den = 1, 0, 0, 0
             maxFrac = 0
             for i in range(n):
-                while j < n and arr[i] >= arr[j] * mid:
+                while j<n and arr[i] >= arr[j] * mid:
                     j += 1
+                if j == n: break
                 
-                total += n - j
+                total += n - j 
 
-                if j < n and maxFrac < arr[i] * 1.0 / arr[j]:
-                    maxFrac = arr[i] * 1.0 / arr[j]
+                if maxFrac < arr[i] / arr[j]:
+                    maxFrac = arr[i] / arr[j]
                     num, den = i, j
-
+            
             if total == k:
-                res = [arr[num], arr[den]]
-                break
-
+                return [arr[num], arr[den]]
+            
             if total > k:
                 right = mid
             else:
                 left = mid
-
-        return res
+        
