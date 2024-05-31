5905ee03d6109df6b1e58606d0a475eaecfc5426
idk-who <idontknowwhodoi@gmail.com>
2024-05-31 18:40:34 +0530

    Time: 58 ms (57.78%) | Memory: 18.4 MB (76.83%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+
+        for i in nums:
+            xor ^= i
+        
+        diff = xor & -xor
+
+        dig1 = 0
+        dig2 = 0
+
+        for i in nums:
+            if i & diff:
+                dig1 ^= i
+            else:
+                dig2 ^= i
+        
+        return [dig1, dig2]
