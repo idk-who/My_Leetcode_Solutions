38b54323333125bfc454ae94b35f19f32424d2e5
idk-who <idontknowwhodoi@gmail.com>
2024-06-02 22:45:23 +0530

    Time: 1160 ms (100.00%) | Memory: 31.5 MB (100.00%) - LeetSync

---
+++ 3436-find-subarray-with-bitwise-and-closest-to-k/find-subarray-with-bitwise-and-closest-to-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        poss = set()
+        ans = abs(nums[0]-k)
+        for i in range(len(nums)):
+            next_pos = set()
+            for p in poss:
+                next_pos.add(p&nums[i])
+            next_pos.add(nums[i])
+
+            for i in next_pos:
+                ans = min(ans, abs(i-k))
+            
+            poss = next_pos
+        
+        return ans
+            
