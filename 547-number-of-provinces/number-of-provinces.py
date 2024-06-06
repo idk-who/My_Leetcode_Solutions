84e106b5bb4d3acd3ff40ddc519365c27dd6cff2
idk-who <idontknowwhodoi@gmail.com>
2024-06-06 23:14:21 +0530

    Time: 193 ms (22.01%) | Memory: 16.9 MB (99.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,17 +1,35 @@
+class UnionFind:
+    def __init__(self, n):
+        self.count = n
+        self.parents = [i for i in range(n)]
+        self.size = [1 for i in range(n)]
+
+    def find(self, n):
+        while self.parents[n] != n: 
+            self.parents[n] = self.parents[self.parents[n]]
+            n = self.parents[n]
+        return n
+    
+    def union(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb: return
+        if self.size[pa] > self.size[pb]:
+            self.parents[pb] = pa
+            self.size[pa] += self.size[pb]
+        else:
+            self.parents[pa] = pb
+            self.size[pb] += self.size[pa]
+        self.count -= 1
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        def dfs(i, j):
-            isConnected[i][j] = 0
-            for k in range(n):
-                if isConnected[j][k]:
-                    dfs(j, k)
+        
+        uf = UnionFind(n)
 
-        ans = 0
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j]:
-                    ans += 1
-                    dfs(i, j)
-        
-        return ans
+                    uf.union(i, j)
+
+        return uf.count
