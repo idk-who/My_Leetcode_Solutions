520e2defbd13fc972b834992e21c7064a8189bbe
idk-who <idontknowwhodoi@gmail.com>
2024-06-07 18:40:27 +0530

    Time: 66 ms (99.52%) | Memory: 24.9 MB (81.26%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -2,14 +2,14 @@ class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         d = set(dictionary)
         minl = min(map(len, d))
-        maxl = min(map(len, d))
+        maxl = max(map(len, d))
         
         ans = []
         
         for word in sentence.split():
             lword = len(word)
             mword = word
-            for i in range(min(minl, lword), max(maxl, lword)):
+            for i in range(min(minl, lword), min(maxl, lword)+1):
                 if word[:i] in d:
                     mword = word[:i]
                     break
