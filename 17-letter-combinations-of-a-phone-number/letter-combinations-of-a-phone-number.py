eb946459c4815c855cca790cfe9e253577da7c08
idk-who <idontknowwhodoi@gmail.com>
2024-06-10 10:02:37 +0530

    Time: 35 ms (55.58%) | Memory: 16.4 MB (80.47%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -17,14 +17,16 @@ class Solution:
         ]
         digits = list(map(int, list(digits)))
         # print(digits)
-        self.helper(digits, 0, "")
+        self.helper(digits, 0, [])
         return self.ans
 
     def helper(self, digits, ptr, carrier):
         if ptr == len(digits):
-            self.ans.append(carrier)
+            self.ans.append("".join(carrier))
             return
         
         # print(ptr, digits)
         for c in self.map[digits[ptr]]:
-            self.helper(digits, ptr+1, carrier+c)
+            carrier.append(c)
+            self.helper(digits, ptr+1, carrier)
+            carrier.pop()
