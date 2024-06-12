2591b8c83c060193cf5eec7db6fccf7da1ba7fd3
idk-who <idontknowwhodoi@gmail.com>
2024-06-12 19:20:18 +0530

    Time: 35 ms (73.49%) | Memory: 16.5 MB (79.02%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,8 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(len(nums)-1):
-                if nums[j+1] < nums[j]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-        
+        # dutch partitioning problem
+
+        red, white, blue = 0, 0, len(nums)-1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+
