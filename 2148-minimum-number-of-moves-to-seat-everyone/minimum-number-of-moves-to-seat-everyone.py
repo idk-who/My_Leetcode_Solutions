37351ea321ec84979eb52ca6424d2e6528620391
idk-who <idontknowwhodoi@gmail.com>
2024-06-13 11:07:30 +0530

    Time: 53 ms (90.83%) | Memory: 16.5 MB (82.54%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -3,9 +3,9 @@ class Solution:
         freq = [0]*101
         n = len(seats)
 
-        for i in range(n):
-            freq[seats[i]]+=1
-            freq[students[i]]-=1
+        for a, b in zip(seats, students):
+            freq[a]+=1
+            freq[b]-=1
         
         ans = 0
         for i in range(1,101):
