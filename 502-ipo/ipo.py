22e76ced857bbe49e06cda20cb4f8480b3a9c46b
idk-who <idontknowwhodoi@gmail.com>
2024-06-15 22:54:23 +0530

    Time: 813 ms (24.80%) | Memory: 41.3 MB (39.44%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -13,25 +13,18 @@ class Solution:
         for c, p in zip(capital, profits):
             heappush(cp, (c, p))
 
-        ans = w
         h = []
         while k and w >= 0:
             while len(cp) > 0 and cp[0][0] <= w:
                 c, p = heappop(cp)
                 heappush(h, (-p, c))
             
-            while len(h) > 0 and h[0][1] > w:
-                p, c = heappop(h)
-                heappush(cp, (c, -p))
-            
             if len(h) == 0:
                 break
 
             p, c = heappop(h)
-            p = -p
-            ans += p
-            w = w + p
+            w -= p
 
             k -= 1
         
-        return ans
+        return w
