55bdb47a36f56b081d301c76fa39ee09a69dd03c
idk-who <idontknowwhodoi@gmail.com>
2024-06-25 00:12:11 +0530

    Time: 42 ms (16.52%) | Memory: 16.4 MB (73.37%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,11 @@
 class Solution:
     def rob(self, A):
-        if len(A) <= 2:
-            return max(A)
+        if len(A) < 2:
+            return A[0]
         one = A[0]
         two = max(one, A[1])
-        three = max(one + A[2], two)
 
-        for i in range(3, len(A)):
-            three, two, one = max(A[i] + two, three), three, two
-            print(three)
+        for i in range(2, len(A)):
+            two, one = max(A[i] + one, two), two
         
-        return three
+        return two
