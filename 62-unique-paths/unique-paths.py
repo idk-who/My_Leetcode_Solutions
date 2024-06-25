c4f18cca578f25077717c3c4e0525fc07452539e
idk-who <idontknowwhodoi@gmail.com>
2024-06-25 11:28:03 +0530

    Time: 39 ms (31.95%) | Memory: 16.6 MB (31.92%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,11 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m > n: m, n = n, m
+        # if m > n: m, n = n, m
 
         dp = [1]*m
 
         for _ in range(1, n):
             for j in range(1, m):
-                dp[j] = dp[j] + dp[j-1]
+                dp[j] += dp[j-1]
         
         return dp[-1]
