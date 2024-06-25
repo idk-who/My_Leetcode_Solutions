29d04fdebdcd34d92f86ac706ce3483cb27f11fa
idk-who <idontknowwhodoi@gmail.com>
2024-06-25 15:37:30 +0530

    Time: 57 ms (75.52%) | Memory: 17.4 MB (92.28%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -8,7 +8,7 @@ class Solution:
 
         for i in range(1, n):
             temp[0] = triangle[i][0] + dp[0]
-            temp[i] =  triangle[i][i] + dp[i-1]
+            temp[i] = triangle[i][i] + dp[i-1]
             for j in range(1, i):
                 temp[j] = triangle[i][j] + min(dp[j-1], dp[j])
             dp[:] = temp
