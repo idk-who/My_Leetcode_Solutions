5cf13a11df75d9d21585356a6ac31fec87b50864
idk-who <idontknowwhodoi@gmail.com>
2024-06-28 21:44:09 +0530

    Time: 1213 ms (75.41%) | Memory: 42 MB (60.66%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maximumImportance(self, n, roads):
+        degree = [0] * n
+        
+        for road in roads:
+            degree[road[0]] += 1
+            degree[road[1]] += 1
+        
+        cities = list(range(n))
+        cities.sort(key=lambda x: -degree[x])
+        
+        total_importance = 0
+        for i in range(n):
+            total_importance += (n - i) * degree[cities[i]]
+        
+        return total_importance
+        
