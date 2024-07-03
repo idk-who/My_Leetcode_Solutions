7bcfb3cfa9514ae84bd9f30a6a011de276acfbbc
idk-who <idontknowwhodoi@gmail.com>
2024-07-03 17:13:58 +0530

    Time: 42 ms (21.61%) | Memory: 16.6 MB (67.23%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,9 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
-        for i in range(len(nums)):
-            ans += [ele+[nums[i]] for ele in ans]
-
+        ans = []
+        set_size = 2**(len(nums))
+        for i in range(set_size):
+            temp = []
+            for j in range(len(nums)):
+                if i & (1 << j):
+                    temp.append(nums[j])
+            ans.append(temp)
         return ans
 
-    
