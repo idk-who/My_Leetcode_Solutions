5db10e335d67282476a8f1cbe564c95adb5d872f
idk-who <idontknowwhodoi@gmail.com>
2024-07-06 16:09:56 +0530

    Time: 76 ms (99.26%) | Memory: 16.6 MB (13.18%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -2,27 +2,27 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for i in range(0, 9, 3):
             for j in range(0, 9, 3):
-                s = set()
+                s = [False]*10
                 for k in range(i, i+3):
                     for l in range(j, j+3):
-                        if board[k][l] in s:
-                            return False
                         if board[k][l] != '.':
-                            s.add(board[k][l])
+                            if s[int(board[k][l])]:
+                                return False
+                            s[int(board[k][l])] = True
         for i in range(9):
-            s = set()
+            s = [False]*10
             for j in range(9):
-                if board[i][j] in s:
-                    return False
-                if board[i][j] != ".":
-                    s.add(board[i][j])
-                    
+                if board[i][j] != '.':
+                    if s[int(board[i][j])]:
+                        return False
+                    s[int(board[i][j])] = True
+
         for i in range(9):
-            s = set()
+            s = [False]*10
             for j in range(9):
-                if board[j][i] in s:
-                    return False
-                if board[j][i] != ".":
-                    s.add(board[j][i])
+                if board[j][i] != '.':
+                    if s[int(board[j][i])]:
+                        return False
+                    s[int(board[j][i])] = True
         
         return True
