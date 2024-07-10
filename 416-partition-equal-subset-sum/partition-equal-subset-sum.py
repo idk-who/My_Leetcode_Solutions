0a9597edeefc64d930a8652aac83931a1e46ae29
idk-who <idontknowwhodoi@gmail.com>
2024-07-10 16:45:15 +0530

    Time: 721 ms (48.80%) | Memory: 16.7 MB (83.08%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -7,12 +7,14 @@ class Solution:
         else:
             k = su // 2
             n = len(nums)
-            dp = [[False]*(k+1) for i in range(n)]
-            dp[0][0] = True
+            dp = [False]*(k+1)
+            dp[0] = True
             if nums[0] <= k:
-                dp[0][nums[0]] =  True
+                dp[nums[0]] =  True
+            new_dp = [False]*(k+1)
+
             for i in range(1, n):
-                dp[i][0] = True
-                for t in range(1, k+1):
-                    dp[i][t] = dp[i-1][t] or (dp[i-1][t-nums[i]] if nums[i] <= t else False)
-            return dp[-1][-1]
+                for t in range(k+1):
+                    new_dp[t] = dp[t] or (dp[t-nums[i]] if nums[i] <= t else False)
+                dp[:] = new_dp
+            return dp[-1]
