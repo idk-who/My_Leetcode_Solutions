8e96e61528cdcac6f2e2e110f8d558f58a359864
idk-who <idontknowwhodoi@gmail.com>
2024-07-10 16:39:38 +0530

    Time: 1042 ms (38.93%) | Memory: 32 MB (47.35%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         su = sum(nums)
-        dp = dict()
-        def recurse(nums, ptr, s):
-            if s == 0:
-                return True
-            if ptr == len(nums):
-                return False
-            if (ptr, s) not in dp:
-                dp[(ptr, s)] = recurse(nums, ptr+1, s) or recurse(nums, ptr+1, s-nums[ptr])
-            return dp[(ptr, s)]
 
         if su & 1:
             return False
         else:
-            return recurse(nums, 0, su//2)
+            k = su // 2
+            n = len(nums)
+            dp = [[False]*(k+1) for i in range(n)]
+            dp[0][0] = True
+            if nums[0] <= k:
+                dp[0][nums[0]] =  True
+            for i in range(1, n):
+                dp[i][0] = True
+                for t in range(1, k+1):
+                    dp[i][t] = dp[i-1][t] or (dp[i-1][t-nums[i]] if nums[i] <= t else False)
+            return dp[-1][-1]
