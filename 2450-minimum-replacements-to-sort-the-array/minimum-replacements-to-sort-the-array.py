5c4ebf9f3594f4cecb3266bb75a2295fca7e5c1e
idk-who <idontknowwhodoi@gmail.com>
2024-07-15 23:43:27 +0530

    Time: 529 ms (5.40%) | Memory: 31.5 MB (14.19%) - LeetSync

---
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumReplacement(self, arr: List[int]) -> int:
+        operations = 0
+        mi = float("inf")
+        for i in range(len(arr)-1, -1, -1):
+            print(mi)
+            if arr[i] < mi:
+                mi = arr[i]
+            elif arr[i] > mi:
+                partitions = (arr[i]//mi) + ((arr[i]%mi)!=0)
+                operations += partitions - 1
+                left_ele = arr[i]//partitions
+                mi = left_ele
+        return operations
+            
