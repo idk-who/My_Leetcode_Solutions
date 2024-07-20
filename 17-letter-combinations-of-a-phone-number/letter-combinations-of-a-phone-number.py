e441de8fe055d04f04a4ec5318f4a08e3e087563
idk-who <idontknowwhodoi@gmail.com>
2024-07-20 18:32:38 +0530

    Time: 24 ms (98.09%) | Memory: 16.5 MB (25.72%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,32 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0:
-            return []
-        self.ans = []
-        self.map = [
-            '',
-            '',
-            'abc',
-            'def',
-            'ghi',
-            'jkl',
-            'mno',
-            'pqrs',
-            'tuv',
-            'wxyz'
+        if digits == "": return []
+        mapping = [
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
         ]
-        digits = list(map(int, list(digits)))
-        # print(digits)
-        self.helper(digits, 0, [])
-        return self.ans
+        def rec(digits, ptr, temp, ans):
+            if ptr == len(digits):
+                ans.append(temp)
+                return
+            for c in mapping[int(digits[ptr])]:
+                rec(digits, ptr+1, temp+c, ans)
 
-    def helper(self, digits, ptr, carrier):
-        if ptr == len(digits):
-            self.ans.append("".join(carrier))
-            return
-        
-        # print(ptr, digits)
-        for c in self.map[digits[ptr]]:
-            carrier.append(c)
-            self.helper(digits, ptr+1, carrier)
-            carrier.pop()
+        ans = []
+        rec(digits, 0, "", ans)
+        return ans
