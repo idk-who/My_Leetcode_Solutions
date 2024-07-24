5489569ac51d852db669b22fd39a456bbff07ff3
idk-who <idontknowwhodoi@gmail.com>
2024-07-25 03:21:38 +0530

    Time: 61 ms (26.05%) | Memory: 17.4 MB (17.27%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,24 +1,22 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
+        adj = [[] for _ in range(len(edges)+1)]
 
-        def BFS(node, par):
-            visited = set([node])
-            q = deque([(node, par)])
-            while q:
-                node, parent = q.popleft()
-                for v in adj[node]:
-                    if v not in visited:
-                        visited.add(v)
-                        q.append((v, node))
-                    elif v != parent:
+        def DFS(node, parent, visited):
+            visited[node] = True
+            for v in adj[node]:
+                if not visited[v]:
+                    if DFS(v, node, visited):
                         return True
+                elif v != parent:
+                    return True
             return False
         
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-            if BFS(u, v):
+            visited = [False]*(len(edges)+1)
+            if DFS(u, v, visited):
                 return [u, v]
         
         return [-1, -1]
