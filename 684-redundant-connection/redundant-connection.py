c5f161c1c526e18ab9113b4d5aa9c26ab6f6dc69
idk-who <idontknowwhodoi@gmail.com>
2024-07-25 03:31:55 +0530

    Time: 58 ms (41.34%) | Memory: 17 MB (42.79%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,22 +1,31 @@
+class UnionFind():
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1]*n
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv: return False
+        if self.size[pu] > self.size[pv]:
+            self.parent[pv] = pu
+            self.size[pu] += self.size[pv]
+        else:
+            self.parent[pu] = pv
+            self.size[pv] += self.size[pu]
+        return True
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         adj = [[] for _ in range(len(edges)+1)]
 
-        def DFS(node, parent, visited):
-            visited[node] = True
-            for v in adj[node]:
-                if not visited[v]:
-                    if DFS(v, node, visited):
-                        return True
-                elif v != parent:
-                    return True
-            return False
+        uf = UnionFind(len(edges)+1)
         
         for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            visited = [False]*(len(edges)+1)
-            if DFS(u, v, visited):
+            if not uf.union(u, v):
                 return [u, v]
         
         return [-1, -1]
