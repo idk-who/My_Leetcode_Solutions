b1d38158116a9344643320970cefa41ae137236d
idk-who <idontknowwhodoi@gmail.com>
2024-07-27 23:23:17 +0530

    Time: 917 ms (89.34%) | Memory: 18.3 MB (58.38%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
@@ -2,6 +2,8 @@ class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
         n = len(original)
         adj = [[float("inf")]*26 for i in range(26)]
+        for i in range(26): adj[i][i] = 0
+
         off = ord('a')
         for i in range(n):
             adj[ord(original[i])-off][ord(changed[i])-off] = min(
@@ -9,9 +11,6 @@ class Solution:
                 cost[i]
             )
         
-        for i in range(26):
-            adj[i][i] = 0
-
         for k in range(26):
             for u in range(26):
                 if adj[u][k] != float("inf"):
@@ -21,14 +20,9 @@ class Solution:
                             adj[u][k] + adj[k][v]
                         )
 
-        for u in range(26):
-            for v in range(26):
-                if adj[u][v] == float("inf"):
-                    adj[u][v] = -1
-
         total_cost = 0
 
         for s, t in zip(source, target):
-            if adj[ord(s)-off][ord(t)-off] == -1: return -1
+            if adj[ord(s)-off][ord(t)-off] == float("inf"): return -1
             total_cost += adj[ord(s)-off][ord(t)-off]
         return total_cost
