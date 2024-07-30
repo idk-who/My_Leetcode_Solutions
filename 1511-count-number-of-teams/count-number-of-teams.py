4dd4cadf23651022e73553c572653d93c84f5038
idk-who <idontknowwhodoi@gmail.com>
2024-07-30 10:51:44 +0530

    Time: 107 ms (95.72%) | Memory: 17.9 MB (6.16%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -1,23 +1,37 @@
+class FT:
+    def __init__(self, n):
+        self.bit = [0]*n
+    def update(self, ind, x):
+        while ind < len(self.bit):
+            self.bit[ind] += x
+            ind |= ind + 1
+    def query(self, end):
+        ans = 0
+        while end:
+            ans += self.bit[end - 1]
+            end &= end - 1
+        return ans
+
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        n = len(rating)
-        ans = 0
-        for i, ele in enumerate(rating):
-            left_less = left_great = right_less = right_great = 0
+        # fenwick tree without mask
+        ma = max(rating)
+        lft = FT(ma+1)
+        rft = FT(ma+1)
 
-            for j in range(0, i):
-                if rating[j]<ele:
-                    left_less += 1
-                else:
-                    left_great += 1
-            
-            for j in range(i+1, n):
-                if rating[j]<ele:
-                    right_less += 1
-                else:
-                    right_great += 1
-            
-            ans += (left_less * right_great)
-            ans += (left_great * right_less)
+        for r in rating: rft.update(r, 1)
         
-        return ans
+        cnt = 0
+        for r in rating:
+            rft.update(r, -1)
+
+            ll = lft.query(r)
+            lg = lft.query(ma+1)-lft.query(r+1)
+            rl = rft.query(r)
+            rg = rft.query(ma+1)-rft.query(r+1)
+
+            cnt += (ll*rg)+(lg*rl)
+
+            lft.update(r, 1)
+        
+        return cnt
