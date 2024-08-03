28298289eb1afd2e245112e06de3c83b22e35257
idk-who <idontknowwhodoi@gmail.com>
2024-08-03 16:38:23 +0530

    Time: 431 ms (52.30%) | Memory: 27.9 MB (10.35%) - LeetSync

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.py
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.py
@@ -1,28 +1,18 @@
 class Solution:
-    def maxNumOfSubstrings(self, string: str) -> List[str]:
-        fst = { s:i for i, s in reversed(list(enumerate(string))) }
-        lst = { s:i for i, s in enumerate(string) }
-
-        intervals = []
-
-        for c in set(string):
-            b = fst[c]
-            e = lst[c]
-            ptr = b
-            while ptr < e and b == fst[c]:
-                b = min(b, fst[string[ptr]])
-                e = max(e, lst[string[ptr]])
-                ptr += 1
-            if b == fst[c]:
-                intervals.append([b, e])
+    def maxNumOfSubstrings(self, s):
+        fst = { c : i for i, c in reversed(list(enumerate(s))) }
+        lst = { c : i for i, c in enumerate(s) }
         
-        intervals.sort(key = lambda x: x[1])
-
-        ans = []
-        prev = -1
-        for b, e in intervals:
-            if b > prev:
+        ans, prev = [], -1
+        for i in sorted(lst.values()):
+            b, e = fst[s[i]], lst[s[i]]
+            j = e
+            while j >= b and b > prev and e == i:
+                b = min(b, fst[s[j]])
+                e = max(e, lst[s[j]])
+                j -= 1
+            if b > prev and e == i:
+                ans.append(s[b:e + 1])
                 prev = e
-                ans.append(string[b:e+1])
         
         return ans
