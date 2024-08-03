9af34b3001f6f7d5b7e4034c4d1cdeac41578cb6
idk-who <idontknowwhodoi@gmail.com>
2024-08-03 17:34:28 +0530

    Time: 272 ms (85.27%) | Memory: 16.6 MB (37.82%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,42 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) <= 1: return s
-        def odd_pal(st, i):
-            s = i-1
-            e = i+1
-            ans = 1
-            while s >= 0 and e < len(st):
-                if st[s] == st[e]:
-                    ans += 2
-                    s -= 1
-                    e += 1
-                else:
-                    break
-            return ans, s, e
+
+        def pal(st, s, e):
+            while s >= 0 and e < len(st) and st[s] == st[e]:
+                s -= 1
+                e += 1
+            return st[s+1:e]
         
-        def even_pal(st, i):
-            s = i
-            e = i+1
-            ans = 0
-            while s >= 0 and e < len(st):
-                if st[s] == st[e]:
-                    ans += 2
-                    s -= 1
-                    e += 1
-                else:
-                    break
-            return ans, s, e
-        
-        ans = 0
-        ans_s = ""
+        ans = ""
         for i in range(len(s)-1):
-            n1, s1, e1 = odd_pal(s, i)
-            n2, s2, e2 = even_pal(s, i)
-            if n1 > ans:
-                ans = n1
-                ans_s = s[s1+1:e1]
-            if n2 > ans:
-                ans = n2
-                ans_s = s[s2+1:e2]
+            odd_pal = pal(s, i, i)
+            even_pal = pal(s, i, i+1)
+            
+            if len(odd_pal) > len(ans):
+                ans = odd_pal
+            if len(even_pal) > len(ans):
+                ans = even_pal
         
-        return ans_s
+        return ans
