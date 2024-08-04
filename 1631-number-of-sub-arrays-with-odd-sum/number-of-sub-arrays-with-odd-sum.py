21c6998a2589d3fdf34b4d094300970697ef41a0
idk-who <idontknowwhodoi@gmail.com>
2024-08-05 00:49:45 +0530

    Time: 846 ms (83.41%) | Memory: 20.2 MB (93.17%) - LeetSync

---
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        cnt = 0
+        odds = 0
+        evens = 0
+
+        for i in arr:
+            if i & 1:
+                evens, odds = odds, evens + 1
+            else:
+                evens, odds = evens + 1, odds
+            
+            cnt = (cnt + odds) % (10**9 + 7)
+        
+        return cnt
+
