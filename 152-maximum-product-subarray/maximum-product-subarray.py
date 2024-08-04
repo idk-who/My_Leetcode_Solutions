caebf062da2ed6b90eb5d7d097e50a96c4da5f74
idk-who <idontknowwhodoi@gmail.com>
2024-08-05 02:07:31 +0530

    Time: 70 ms (67.77%) | Memory: 17.1 MB (27.60%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,28 +1,18 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        prod_till_first_neg = 1
-        first_neg_ind = -1
-        prod = 1
-        ans = nums[0]
+        imax = nums[0]
+        imin = nums[0]
+        r = nums[0]
 
-        for ind, i in enumerate(nums):
-            prod *= i
-            if prod_till_first_neg > 0:
-                prod_till_first_neg *= i
-                first_neg_ind = ind
-            if i == 0:
-                prod_till_first_neg = 1
-                first_neg_ind = -1
+        for i in nums[1:]:
+            candidates = (i, imax*i, imin*i)
 
-            
-            if prod < 0:
-                ans = max(ans, prod//prod_till_first_neg if first_neg_ind != ind else prod)
-            else:
-                ans = max(ans, prod)
-            
-            if prod == 0:
-                prod = 1
-        
-        return ans
+            imax = max(candidates)
+            imin = min(candidates)
+
+            r = max(r, imax)
+
+        return r
+
 
             
