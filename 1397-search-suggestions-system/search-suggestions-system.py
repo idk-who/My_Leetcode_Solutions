19b98eee67b34bd533a4eb2d8f9eff2563a327bc
idk-who <idontknowwhodoi@gmail.com>
2024-08-05 01:17:47 +0530

    Time: 62 ms (95.72%) | Memory: 19.7 MB (71.46%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -0,0 +1,36 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+
+        def BS(word):
+            l = 0 
+            r = len(products)
+
+            while l < r:
+                m = l + (r-l)//2
+                if products[m] == word:
+                    return m
+                elif word < products[m]:
+                    r = m
+                else:
+                    l = m + 1
+            
+            return l
+        
+        ans = []
+
+        temp = ''
+        for c in searchWord:
+            temp += c
+
+            ptr = BS(temp)
+
+            lst = []
+
+            while len(lst) < 3 and ptr < len(products) and products[ptr].startswith(temp):
+                lst.append(products[ptr])
+                ptr += 1
+            
+            ans.append(lst)
+        
+        return ans
