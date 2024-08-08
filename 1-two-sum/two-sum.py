9381cc61883910373db2d6d2bb88f029c19a24ec
idk-who <idontknowwhodoi@gmail.com>
2024-08-08 15:20:16 +0530

    Time: 70 ms (42.43%) | Memory: 18.1 MB (7.72%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,19 @@
-from bisect import bisect
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = dict()
+        temp = list(enumerate(nums))
+        temp.sort(key = lambda x: x[1])
+        nums.sort()
 
-        for i in range(len(nums)):
-            if target-nums[i] in d:
-                return [d[target-nums[i]], i]
-            d[nums[i]] = i
+        p1 = 0
+        p2 = len(nums)-1
+
+        while p1 < p2:
+            if nums[p1] + nums[p2] == target:
+                return [temp[p1][0], temp[p2][0]]
+            elif nums[p1] + nums[p2] > target:
+                p2 -= 1
+            else:
+                p1 += 1
+        
+
         
