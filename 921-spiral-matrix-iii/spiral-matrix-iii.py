b0543c4654830c4c13a9532f4e74c1ce8b70de92
idk-who <idontknowwhodoi@gmail.com>
2024-08-08 22:42:51 +0530

    Time: 80 ms (71.09%) | Memory: 17.7 MB (74.61%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -1,45 +1,23 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        di = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        p_di = 0
-
         ans = [(rStart, cStart)]
-
         iterations = max(
             abs(rStart - rows),
             abs(rStart - 0),
             abs(cStart - cols),
             abs(cStart - 0)
         )
-
+        di = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         cnt = 1
         i, j = rStart, cStart
 
         for __ in range(iterations+1):
-            for _ in range(cnt):
-                i, j = i+di[p_di][0], j+di[p_di][1] 
-                if 0 <= i < rows and 0 <= j < cols:
-                    ans.append([i, j])
-            p_di += 1
-            for _ in range(cnt):
-                i, j = i+di[p_di][0], j+di[p_di][1]
-                if 0 <= i < rows and 0 <= j < cols:
-                    ans.append([i, j])
-            p_di += 1
-            cnt += 1
-
-            for _ in range(cnt):
-                i, j = i+di[p_di][0], j+di[p_di][1]
-                if 0 <= i < rows and 0 <= j < cols:
-                    ans.append([i, j])
-            p_di += 1
-            for _ in range(cnt):
-                i, j = i+di[p_di][0], j+di[p_di][1]
-                if 0 <= i < rows and 0 <= j < cols:
-                    ans.append([i, j])
-            p_di += 1
-            cnt += 1
-
-            p_di = 0
+            for p_di in range(4):
+                for _ in range(cnt):
+                    i, j = i+di[p_di][0], j+di[p_di][1] 
+                    if 0 <= i < rows and 0 <= j < cols:
+                        ans.append([i, j])
+                if p_di == 1 or p_di == 3:
+                    cnt += 1
         
         return ans
