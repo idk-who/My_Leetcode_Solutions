6adadfb68842b72626f12e85e1e6cb86b72782c5
idk-who <idontknowwhodoi@gmail.com>
2024-08-17 02:33:12 +0530

    Time: 111 ms (16.07%) | Memory: 17.4 MB (52.08%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,19 +4,21 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n, m = len(grid), len(grid[0])
-        rows = [False]*n
-        cols = [False]*m
+        col_zero = 1
 
         for i in range(n):
-            for j in range(m):
+            if grid[i][0] == 0: col_zero = 0
+            for j in range(1, m):
                 if grid[i][j] == 0:
-                    rows[i] = True
-                    cols[j] = True
-        
+                    grid[i][0] = 0
+                    grid[0][j] = 0
 
-        for i in range(n):
-            for j in range(m):
-                if rows[i] or cols[j]:
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, 0, -1):
+                if grid[i][0] == 0 or grid[0][j] == 0:
                     grid[i][j] = 0
+            if col_zero == 0: grid[i][0] = 0
+        
+        
 
         
