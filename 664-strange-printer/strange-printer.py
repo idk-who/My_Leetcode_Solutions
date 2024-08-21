5fd660cfa95658fc5b8bf21f0591cf17d0109e41
idk-who <idontknowwhodoi@gmail.com>
2024-08-21 23:39:31 +0530

    Time: 188 ms (87.91%) | Memory: 19.3 MB (27.47%) - LeetSync

--- 664-strange-printer/strange-printer.py
+++ 664-strange-printer/strange-printer.py
@@ -1,22 +1,17 @@
-class Solution():
-    def strangePrinter(self, s):
-        cache = dict()
-
-        def solve(s):
-            if not s:
-                return 0
-            if s in cache:
-                return cache[s]
-            # cost to simply insert last character
-            cost = solve(s[0:-1]) + 1
-            # what if last character could be inserted for free just by reusing previous step that already prints it?
-			# . . . . . A . . . . A
-			# |---------| |-----| last character is free
-            char_to_insert = s[-1]
-            for i, c in enumerate(s[:-1]):
-                if c == char_to_insert:
-                    cost = min(cost, solve(s[0:i + 1]) + solve(s[i + 1:-1]))
-            cache[s] = cost
-            return cost
-
-        return solve(s)
+class Solution:
+    def strangePrinter(self, s: str) -> int:
+        # String processing refer to lee215 - https://leetcode.com/problems/strange-printer/discuss/106794/One-suggestion-for-all-solutions
+        S = ''.join(a for a, b in zip(s, '#' + s) if a != b)
+        # S = re.sub(r'(.)\1*', r'\1', s)
+        
+        @lru_cache(None)
+        # dp(i, j) is the number of turns needed to print S[i:j]
+        def dp(i, j):
+            if i == j: return 0
+            ans = dp(i, j-1) + 1
+            for k in range(i, j-1):
+                if S[k] == S[j-1]:
+                    ans = min(ans, dp(i, k+1) + dp(k+1, j-1))
+            return ans
+    
+        return dp(0, len(S))   
