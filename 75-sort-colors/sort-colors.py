75317a444f5b460c7225f8b9297cad6270603bc7
idk-who <idontknowwhodoi@gmail.com>
2024-09-01 03:06:39 +0530

    Time: 30 ms (92.81%) | Memory: 16.5 MB (23.27%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,7 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        zero = one = two = 0
 
+        for i in nums:
+            if i == 0: zero += 1
+            elif i == 1: one += 1
+            else: two += 1
+
+        for i in range(zero):
+            nums[i] = 0
         
+        for i in range(zero, zero+one):
+            nums[i] = 1
+        
+        for i in range(zero+one, len(nums)):
+            nums[i] = 2
+        
+        
+
+
         
