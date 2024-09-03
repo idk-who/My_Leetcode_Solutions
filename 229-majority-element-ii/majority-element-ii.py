57d7d1337bdabfb25483fa354181bb332cf6861a
idk-who <idontknowwhodoi@gmail.com>
2024-09-04 02:11:42 +0530

    Time: 102 ms (37.16%) | Memory: 18 MB (21.46%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,3 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        return [i for i, j in Counter(nums).items() if j > len(nums)/3]
+        ele1 = None
+        cnt1 = 0
+        ele2 = None
+        cnt2 = 0
+
+        for i in nums:
+            if i == ele1:
+                cnt1 += 1
+            elif i == ele2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                ele1 = i
+                cnt1 += 1
+            elif cnt2 == 0:
+                ele2 = i
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        
+        cnt1 = cnt2 = 0
+        for i in nums:
+            if i == ele1: cnt1 += 1
+            if i == ele2: cnt2 += 1
+        
+        ans = []
+        if cnt1 > len(nums)/3: ans.append(ele1)
+        if cnt2 > len(nums)/3: ans.append(ele2)
+
+        return ans
