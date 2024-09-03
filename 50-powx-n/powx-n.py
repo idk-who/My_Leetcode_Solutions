ae868c8046be32ae3d92843fca53aae9be5e11d8
idk-who <idontknowwhodoi@gmail.com>
2024-09-04 01:35:33 +0530

    Time: 42 ms (12.66%) | Memory: 16.7 MB (15.98%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,16 +3,16 @@ class Solution:
         ans = 1
         dp = dict()
 
-        def pow(x, n, dp):
+        def helper(x, n, dp):
             if n == 0: return 1
             if n == 1: return x
             if n == -1: return 1/x
             if n in dp:
                 return dp[n]
-            ans = pow(x, n//2, dp) * pow(x, n//2+(n&1), dp)
+            ans = helper(x, n//2, dp) * helper(x, n//2+(n&1), dp)
             dp[n] = ans
             return ans
         
-        return pow(x, n, dp)
+        return helper(x, n, dp)
 
 
