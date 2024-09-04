65c373f0cc1451e747746d0915792b05eda34636
idk-who <idontknowwhodoi@gmail.com>
2024-09-04 21:57:29 +0530

    Time: 68 ms (41.60%) | Memory: 18 MB (8.80%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = dict()
+        nums = list(enumerate(nums))
+        nums.sort(key = lambda x: x[1])
 
-        for ind, ele in enumerate(nums):
-            if target-ele in d:
-                return d[target-ele], ind
-            d[ele] = ind
-        
+        p1 = 0 
+        p2 = len(nums) - 1
+
+        while p1 < p2:
+            if nums[p1][1] + nums[p2][1] == target:
+                return nums[p1][0], nums[p2][0]
+            elif nums[p1][1] + nums[p2][1] < target:
+                p1 += 1
+            else:
+                p2 -= 1
+                
         return -1, -1
