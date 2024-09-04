140c9a2ce62ea8f6fff03212bdde23a6b37e4efa
idk-who <idontknowwhodoi@gmail.com>
2024-09-04 19:26:22 +0530

    Time: 858 ms (81.21%) | Memory: 23.4 MB (95.46%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.py
+++ 493-reverse-pairs/reverse-pairs.py
@@ -1,35 +1,7 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        def merge(arr, l, m, r):
-            L = arr[l:m+1]
-            R = arr[m+1:r+1]
-
-            p1 = 0
-            p2 = 0
-            p3 = l
-            while p1 < len(L) and p2 < len(R):
-                if L[p1] < R[p2]:
-                    arr[p3] = L[p1]
-                    p1 += 1
-                else:
-                    arr[p3] = R[p2]
-                    p2 += 1    
-                p3 += 1
-            while p1 < len(L):
-                arr[p3] = L[p1]
-                p1 += 1
-                p3 += 1
-            while p2 < len(R):
-                arr[p3] = R[p2]
-                p2 += 1
-                p3 += 1
-
-        def mergeSort(arr, l, r):
-            if l >= r: return 0
-            m = (l+r)//2
-            cnt = mergeSort(arr, l, m)
-            cnt += mergeSort(arr, m+1, r)
-            
+        def countPairs(arr, l, m, r):
+            cnt = 0
             p1 = l
             p2 = m+1
             while p1 <= m and p2 <= r:
@@ -38,7 +10,14 @@ class Solution:
                     p2 += 1
                 else:
                     p1 += 1
+            return cnt
 
+        def mergeSort(arr, l, r):
+            if l >= r: return 0
+            m = (l+r)//2
+            cnt = mergeSort(arr, l, m)
+            cnt += mergeSort(arr, m+1, r)
+            cnt += countPairs(arr, l, m, r)
             arr[l:r+1] = sorted(arr[l:r+1])
 
             return cnt
