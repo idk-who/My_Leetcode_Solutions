0929cdcb8796cf31923e8741f5ef0df7de12df30
idk-who <idontknowwhodoi@gmail.com>
2024-09-04 19:02:46 +0530

    Time: 822 ms (88.83%) | Memory: 24 MB (65.04%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.py
+++ 493-reverse-pairs/reverse-pairs.py
@@ -7,7 +7,6 @@ class Solution:
             p1 = 0
             p2 = 0
             p3 = l
-
             while p1 < len(L) and p2 < len(R):
                 if L[p1] < R[p2]:
                     arr[p3] = L[p1]
@@ -27,14 +26,12 @@ class Solution:
 
         def mergeSort(arr, l, r):
             if l >= r: return 0
-
             m = (l+r)//2
             cnt = mergeSort(arr, l, m)
             cnt += mergeSort(arr, m+1, r)
             
             p1 = l
             p2 = m+1
-
             while p1 <= m and p2 <= r:
                 if arr[p1] > 2 * arr[p2]:
                     cnt += (m - p1 + 1)
@@ -42,7 +39,7 @@ class Solution:
                 else:
                     p1 += 1
 
-            merge(arr, l, m, r)
+            arr[l:r+1] = sorted(arr[l:r+1])
 
             return cnt
 
