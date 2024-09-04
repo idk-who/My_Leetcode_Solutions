1c2d3b53916ecd0deee69ef6cf8ce7afd077eabf
idk-who <idontknowwhodoi@gmail.com>
2024-09-04 23:39:33 +0530

    Time: 624 ms (25.85%) | Memory: 16.6 MB (71.10%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -2,32 +2,20 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
         nums.sort()
-
+        d = {ele:ind for ind, ele in enumerate(nums)}
         ans = []
         for i in range(n):
             if i > 0 and nums[i-1] == nums[i]: continue
             for j in range(i+1, n):
                 if j > i+1 and nums[j-1] == nums[j]: continue
-                p1 = j+1
-                p2 = n-1
-
-                tar = target - nums[i] - nums[j]
-
-                while p1 < p2:
-                    if nums[p1] + nums[p2] == tar:
+                for k in range(j+1, n):
+                    if k > j+1 and nums[k-1] == nums[k]: continue
+                    
+                    tar = target - nums[i] - nums[j] - nums[k]
+                    if tar in d and d[tar] > k:
                         ans.append(
-                            [nums[i],nums[j], nums[p1], nums[p2]]
-                        )
-                        p1 += 1
-                        while p1 < p2 and nums[p1-1] == nums[p1]:
-                            p1 += 1 
-                        p2 -= 1
-                        while p1 < p2 and nums[p2+1] == nums[p2]:
-                            p2 -= 1
-                    elif nums[p1] + nums[p2] < tar:
-                        p1 += 1
-                    else:
-                        p2 -= 1
+                                [nums[i],nums[j], nums[k], tar]
+                            )
             
         return ans
         
