4aa914dfd6a06d17a990dd2a6c3e0a0902b7e0bd
idk-who <idontknowwhodoi@gmail.com>
2024-09-05 23:50:01 +0530

    Time: 1040 ms (26.47%) | Memory: 26.8 MB (69.79%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -1,20 +1,10 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
-        '''
-        m = cMean
-        S = cSum
-        (S + Diff)/(n+m) == mean
-        Diff = (mean*(n+m))-S
-        '''
-        m = len(rolls)
-        S = sum(rolls)
-        diff = (mean*(n+m))-S
-        if diff < n or diff > 6*n: return []
-
-        quo = diff//n
-        rem = diff%n
-        ans = [quo] * n
-        ans = [quo + (i < rem) for i in range(n)]
+        sm = mean * (n + len(rolls)) - sum(rolls)
+        if sm > n * 6 or sm < n:
+            return []
+        ans = [sm // n] * n
+        lo = sm % n
+        for i in range(lo):
+            ans[i] += 1
         return ans
-        
-        
