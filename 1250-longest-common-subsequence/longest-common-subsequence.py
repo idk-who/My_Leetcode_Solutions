d4e00b785450740e160c7a4fdcf7471a34d247f9
idk-who <idontknowwhodoi@gmail.com>
2024-09-08 03:41:05 +0530

    Time: 642 ms (30.50%) | Memory: 24.9 MB (89.55%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,10 +1,11 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[-1]*len(text2) for _ in range(len(text1))]
         def rec(t1, t2, p1, p2, dp):
             if p1 == len(t1) or p2 == len(t2):
                 return 0
-            if (p1, p2) in dp:
-                return dp[(p1, p2)]
+            if dp[p1][p2] != -1:
+                return dp[p1][p2]
             
             if t1[p1] == t2[p2]:
                 ma = 1 + rec(t1, t2, p1+1, p2+1, dp)
@@ -14,7 +15,9 @@ class Solution:
                     rec(t1, t2, p1, p2+1, dp)
                 )
             
-            dp[(p1, p2)] = ma
+            dp[p1][p2] = ma
             return ma
         
-        return rec(text1, text2, 0, 0, dict())
+        return rec(text1, text2, 0, 0, dp)
+
+
