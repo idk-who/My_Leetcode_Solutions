88d96bb20ff7b7517f65a5f48afe69527e9a215c
idk-who <idontknowwhodoi@gmail.com>
2024-09-08 00:31:17 +0530

    Time: 1013 ms (79.76%) | Memory: 17.7 MB (89.52%) - LeetSync

--- 3548-find-the-count-of-good-integers/find-the-count-of-good-integers.py
+++ 3548-find-the-count-of-good-integers/find-the-count-of-good-integers.py
@@ -1,30 +1,25 @@
 class Solution:
     def countGoodIntegers(self, n: int, k: int) -> int:
-        s = set()
         is_odd = (n&1)
-        le = n//2+(n&1)
         def creat_num(i):
             num = str(i)
-            if is_odd:
-                num += str(i)[:-1][::-1]
-            else:
-                num += str(i)[::-1]
-            # print(num)
-            return int(num)
-        print((10**(le-1), 10**le))
+            num += str(i)[::-1][is_odd:]
+            return num
+
+        s = set()
+        le = n//2+(n&1)
         for i in range(10**(le-1), 10**le):
             num = creat_num(i)
-            if num % k == 0:
-                # print("".join(sorted(list(str(num)))))
-                s.add("".join(sorted(str(num))))
+            if int(num) % k == 0:
+                s.add("".join(sorted(num)))
+
         ans = 0
-        print(s)
         for i in s:
             cnt = Counter(i)
-            temp = (n-cnt['0']) * factorial(n-1)
+            perms = (n-cnt['0']) * factorial(n-1)
             for _, j in cnt.items():
-                temp //= factorial(j)
-            ans += temp
+                perms //= factorial(j)
+            ans += perms
         return ans
         
         
