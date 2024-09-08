3f10b8c93b40501257113c90b22bdabd52aad808
idk-who <idontknowwhodoi@gmail.com>
2024-09-09 02:35:09 +0530

    Time: 1136 ms (16.95%) | Memory: 19.3 MB (25.36%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,6 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = dict()
+        dp = [[-1]*(amount+1) for i in range(len(coins))]
 
         def rec(coins, amount, ptr, dp):
             if amount == 0:
@@ -8,17 +8,17 @@ class Solution:
             if amount < 0 or ptr == len(coins):
                 return float("inf")
             
-            if (amount, ptr) in dp:
-                return dp[(amount, ptr)]
+            if dp[ptr][amount] != -1:
+                return dp[ptr][amount]
             
             mi = min(
                 rec(coins, amount, ptr+1, dp),
                 1 + rec(coins, amount-coins[ptr], ptr, dp)
             )
-            dp[(amount, ptr)] = mi
+            dp[ptr][amount] = mi
 
             return mi
-            
+
         ans = rec(coins, amount, 0, dp)
         return ans if ans != float('inf') else -1
 
