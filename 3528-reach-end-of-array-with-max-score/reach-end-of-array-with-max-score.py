354fc690eea99c39fbc0cceaa536215e805dc764
idk-who <idontknowwhodoi@gmail.com>
2024-09-08 23:03:34 +0530

    Time: 1222 ms (100.00%) | Memory: 31.7 MB (100.00%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.py
@@ -1,8 +1,3 @@
 class Solution:
     def findMaximumScore(self, arr: List[int]) -> int:
-        ans = 0 
-        ma = 0
-        for i in arr[:-1]:
-            if i > ma: ma = i
-            ans += ma
-        return ans
+        return sum(accumulate(arr[:-1], max))
