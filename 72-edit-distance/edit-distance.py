6260f719be3559093867f51ea92b385d06fb79df
idk-who <idontknowwhodoi@gmail.com>
2024-09-08 18:01:04 +0530

    Time: 71 ms (92.01%) | Memory: 18.7 MB (77.61%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -2,19 +2,28 @@ class Solution:
     def minDistance(self, w1: str, w2: str) -> int:
         n, m = len(w1), len(w2)
         dp = [[float('inf')]*(m+1) for i in range(n+1)]
-        for i in range(n+1):
-            dp[i][0] = i
-        for j in range(m+1):
-            dp[0][j] = j
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if w1[i-1] == w2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    dp[i][j] = min(
-                        dp[i-1][j], 
-                        dp[i][j-1],
-                        dp[i-1][j-1]
-                        ) + 1
 
-        return dp[-1][-1]
+        def rec(w1, w2, p1, p2, dp):
+            if p1 == len(w1):
+                return m - p2
+            if p2 == len(w2):
+                return n - p1
+            
+            if dp[p1][p2] != float("inf"):
+                return dp[p1][p2]
+            
+            if w1[p1] == w2[p2]:
+                mi = rec(w1, w2, p1+1, p2+1, dp)
+            else:
+                mi = min(
+                    1 + rec(w1, w2, p1+1, p2, dp),
+                    1 + rec(w1, w2, p1, p2+1, dp),
+                    1 + rec(w1, w2, p1+1, p2+1, dp)
+                )
+            
+            dp[p1][p2] = mi
+
+            return mi
+        
+        return rec(w1, w2, 0, 0, dp)
+        
