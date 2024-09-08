605bd60e01b536f177f79a217182cb3e28f6aac2
idk-who <idontknowwhodoi@gmail.com>
2024-09-09 03:17:48 +0530

    Time: 844 ms (39.87%) | Memory: 16.7 MB (94.06%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -3,8 +3,11 @@ class Solution:
         dp = [float('inf')]*(amount+1)
         dp[0] = 0
         coins.sort()
-        for i in range(len(coins)):
-            for j in range(coins[i], amount+1):
-                dp[j] = min(dp[j], 1 + dp[j-coins[i]])
+        for j in range(1, amount+1):
+            for i in range(len(coins)):
+                if coins[i] <= j:
+                    dp[j] = min(dp[j], 1 + dp[j-coins[i]])
+                else:
+                    break
         
         return dp[-1] if dp[-1] != float("inf") else -1
