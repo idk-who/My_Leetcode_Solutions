8f9cceda43eef48d2ec3f6d148264a0337bad5ae
idk-who <idontknowwhodoi@gmail.com>
2024-09-09 22:49:09 +0530

    Time: 700 ms (38.84%) | Memory: 54.4 MB (97.68%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.py
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.py
@@ -6,7 +6,7 @@
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         n, m = m, n
-        ans = [[-2]*m for _ in range(n)]
+        ans = [[-1]*m for _ in range(n)]
 
         def get_val():
             nonlocal head
@@ -22,7 +22,7 @@ class Solution:
         up = 0
         down = n-1
 
-        while left <= right and up <= down:
+        while head and left <= right and up <= down:
             # print("LOOP")
             for j in range(left, right+1):
                 ans[up][j] = get_val()
