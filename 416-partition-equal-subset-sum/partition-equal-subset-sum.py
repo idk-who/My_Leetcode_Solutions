1e796354b4ff61da505a6cdfcfa23a6323bf71cc
idk-who <idontknowwhodoi@gmail.com>
2024-09-10 02:57:23 +0530

    Time: 1287 ms (32.86%) | Memory: 32.2 MB (39.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,21 +4,20 @@ class Solution:
         if su&1:
             return False
         tar = su//2
-        dp = dict()
-        def rec(nums, ptr, tar, dp):
-            if tar == 0:
-                return True
-            if tar < 0 or ptr == len(nums):
-                return False
-            if (ptr, tar) in dp:
-                return dp[(ptr, tar)]
-            
-            val = (
-                rec(nums, ptr+1, tar, dp) or
-                rec(nums, ptr+1, tar-nums[ptr], dp)
-            )
-            dp[(ptr, tar)] = val
-
-            return val
+        n = len(nums)
+        dp = [[False]*(tar+1) for _ in range(n+1)]
         
-        return rec(nums, 0, tar, dp)
+        for i in range(n+1):
+            dp[i][0] = True
+        
+        for i in range(1, n+1):
+            for j in range(1, tar+1):
+                if j >= nums[i-1]:
+                    dp[i][j] = (
+                        dp[i-1][j] or
+                        dp[i-1][j-nums[i-1]]
+                    )
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[-1][-1]
