fb6f9c0afb2581f0ef1d5bf31395e4bf33357dd0
idk-who <idontknowwhodoi@gmail.com>
2024-09-10 03:14:28 +0530

    Time: 894 ms (40.13%) | Memory: 16.6 MB (90.65%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,20 +4,17 @@ class Solution:
         if su&1:
             return False
         tar = su//2
+
         n = len(nums)
-        dp = [[False]*(tar+1) for _ in range(n+1)]
-        
-        for i in range(n+1):
-            dp[i][0] = True
+        dp = [False]*(tar+1)
+        dp[0] = True
         
         for i in range(1, n+1):
-            for j in range(1, tar+1):
+            for j in range(tar, -1, -1):
                 if j >= nums[i-1]:
-                    dp[i][j] = (
-                        dp[i-1][j] or
-                        dp[i-1][j-nums[i-1]]
+                    dp[j] = (
+                        dp[j] or
+                        dp[j-nums[i-1]]
                     )
-                else:
-                    dp[i][j] = dp[i-1][j]
         
-        return dp[-1][-1]
+        return dp[-1]
