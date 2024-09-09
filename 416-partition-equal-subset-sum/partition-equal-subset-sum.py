10e23396fa87762b053db799865adbb520d148a9
idk-who <idontknowwhodoi@gmail.com>
2024-09-10 03:15:03 +0530

    Time: 1507 ms (27.76%) | Memory: 16.6 MB (90.65%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -12,9 +12,6 @@ class Solution:
         for i in range(1, n+1):
             for j in range(tar, -1, -1):
                 if j >= nums[i-1]:
-                    dp[j] = (
-                        dp[j] or
-                        dp[j-nums[i-1]]
-                    )
+                    dp[j] |= dp[j-nums[i-1]]
         
         return dp[-1]
