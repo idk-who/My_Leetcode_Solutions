7d13b8ac1a59b06722e77ce08715b2dcfa28fb6e
idk-who <idontknowwhodoi@gmail.com>
2024-09-10 01:58:01 +0530

    Time: 1699 ms (22.89%) | Memory: 178.7 MB (9.62%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,10 +1,24 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         su = sum(nums)
+        if su&1:
+            return False
+        tar = su//2
+        dp = dict()
+        def rec(nums, ptr, tar, dp):
+            if tar == 0:
+                return True
+            if tar < 0 or ptr == len(nums):
+                return False
+            if (ptr, tar) in dp:
+                return dp[(ptr, tar)]
+            
+            val = (
+                rec(nums, ptr+1, tar, dp) or
+                rec(nums, ptr+1, tar-nums[ptr], dp)
+            )
+            dp[(ptr, tar)] = val
 
-        if su & 1: return False
-        k = su // 2
-        dp = 1
-        for num in nums:
-            dp |= dp << num
-        return dp & (1<<k)
+            return val
+        
+        return rec(nums, 0, tar, dp)
