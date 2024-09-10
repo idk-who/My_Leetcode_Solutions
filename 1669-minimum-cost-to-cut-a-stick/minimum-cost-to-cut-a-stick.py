92f004abbb8dfeb02e822f4c206fac0023077674
idk-who <idontknowwhodoi@gmail.com>
2024-09-10 19:25:49 +0530

    Time: 518 ms (77.29%) | Memory: 17 MB (67.52%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -1,8 +1,25 @@
 class Solution:
-    def minCost(self, n: int, cuts: List[int]) -> int:
+    def minCost(self, le: int, cuts: List[int]) -> int:
         cuts.sort()
-        cuts = [0] + cuts + [n]
-        dp = dict()
+        cuts = [0] + cuts + [le]
+        n = len(cuts)
+        dp = [[float('inf')]*(n+1) for _ in range(n+1)]
+
+        for i in range(n):
+            dp[i][i+1] = 0
+
+        for L in range(2, n):
+            for i in range(1, n+1-L):
+                j = i + L
+                for k in range(i+1, j):
+                    dp[i][j] = min(
+                        dp[i][j],
+                        cuts[j-1] - cuts[i-1] + (
+                            dp[i][k] + dp[k][j]
+                        )
+                    )
+                    
+        return dp[1][n]
         
         def rec(cuts, i, j, dp):
             if j - i == 1:
@@ -24,3 +41,4 @@ class Solution:
             return mi
         
         return rec(cuts, 0, len(cuts)-1, dp)
+
