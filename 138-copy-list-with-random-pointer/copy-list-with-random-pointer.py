0bc7dd36daa057523676fa76f401dd507e914e46
idk-who <idontknowwhodoi@gmail.com>
2024-09-11 22:22:33 +0530

    Time: 32 ms (89.53%) | Memory: 17.5 MB (7.46%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,36 +9,18 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        new_head = Node(-1)
-        
-        new_ptr = new_head
-        old_ptr = head
-        store = dict()
+        store = {}
+        curr = head
+        while curr:
+            store[curr] = Node(curr.val)
+            curr = curr.next
+        store[None] = None # for random pointing to None
 
-        while old_ptr:
-            
-            if old_ptr in store:
-                new_node = store[old_ptr]
-            else:
-                new_node = Node(
-                    old_ptr.val,
-                )
-                store[old_ptr] = new_node
+        curr = head
+        while curr:
+            store[curr].next = store[curr.next]
+            store[curr].random = store[curr.random]
+            curr = curr.next
 
-            new_ptr.next = new_node
+        return store[head]
 
-            if old_ptr.random == None:
-                random = None
-            elif old_ptr.random in store:
-                random = store[old_ptr.random]
-            else:
-                random = Node(
-                    old_ptr.random.val
-                )
-                store[old_ptr.random] = random
-            new_ptr.next.random = random
-
-            old_ptr = old_ptr.next
-            new_ptr = new_ptr.next
-        
-        return new_head.next
