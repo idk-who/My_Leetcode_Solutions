618e3048bfe7b35ddea1a181aa67512516fe59e4
idk-who <idontknowwhodoi@gmail.com>
2024-09-21 23:21:36 +0530

    Time: 71 ms (73.92%) | Memory: 25 MB (20.22%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,3 +1,17 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        return sorted(list(range(1, n+1)), key = lambda x: str(x))
+        result = []
+
+        def rec(num, limit):
+            if num > limit:
+                return
+            result.append(num)
+            for i in range(10):
+                new_num = num*10 + i
+                if new_num > limit: break
+                rec(new_num, limit)
+
+        for i in range(1, 10): 
+            rec(i, n)
+            
+        return result
