3e13677d570c02b61c00dfdb932903ef0e267c74
idk-who <idontknowwhodoi@gmail.com>
2024-09-21 23:54:29 +0530

    Time: 97 ms (41.29%) | Memory: 17 MB (42.16%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,8 +1,9 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         n = len(s)
+        rev_s = s[::-1]
 
         for i in range(n, -1, -1):
-            if s[:i] == s[:i][::-1]:
-                return s[i:][::-1] + s 
+            if s[:i] == rev_s[n-i:]:
+                return rev_s[:n-i] + s 
  
