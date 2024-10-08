7b35d959f3662d32d20b3702b118fad7d2d741ba
idk-who <idontknowwhodoi@gmail.com>
2024-10-08 23:59:28 +0530

    Time: 171 ms (89.04%) | Memory: 27.8 MB (98.61%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,13 +1,15 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        stack_size = 0
-        for ch in s:
-            # If character is opening bracket, increment the stack size.
-            if ch == "[":
-                stack_size += 1
+        mismatched = 0
+        left_par = 0
+
+        for i in s:
+            if i == '[':
+                left_par += 1
             else:
-                # If the character is closing bracket, and we have an opening bracket, decrease
-                # the stack size.
-                if stack_size > 0:
-                    stack_size -= 1
-        return (stack_size + 1) // 2
+                if left_par:
+                    left_par -= 1
+                else:
+                    mismatched += 1
+        
+        return mismatched // 2 + (mismatched & 1)
