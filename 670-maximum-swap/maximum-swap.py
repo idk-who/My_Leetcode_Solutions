cf034d96968547cc12a700643488a02c1b36f0f8
idk-who <idontknowwhodoi@gmail.com>
2024-10-18 00:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (10.80%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,24 +1,25 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        num = list(map(int, list(str(num))))       
-        n = len(num)
+        digits = list(str(num))
+        total_digits = len(digits)
+        digits2indices = dict()
+        for idx, digit in enumerate(digits):
+            if digit not in digits2indices.keys():
+                digits2indices.update({digit: []})
+            digits2indices[digit].append(idx)
 
-        suff_arr = [-1]*n
-
-        ma = -1
-
-        for i in range(n-1, -1, -1):
-            suff_arr[i] = ma
-            ma = max(num[i], ma)
-
-        for i in range(n):
-            if suff_arr[i] > num[i]:
-                ind = n-1
-                while num[ind] != suff_arr[i]:
-                    ind -= 1
-                num[i], num[ind] = num[ind], num[i]
-                break
-            
-        return int("".join(map(str, num)))
+        digits2indices = dict(sorted(digits2indices.items(), reverse=True))
+        current_idx = 0
+        for digit, indices in digits2indices.items():
+            for idx in indices[::-1]:  # Key: put bigger indices to the front if possible.
+                while current_idx < total_digits - 1 and current_idx <= idx:
+                    if digits[current_idx] < digits[idx]:
+                        digits[current_idx], digits[idx] = digits[idx], digits[current_idx]
+                        return int("".join(digits))
+                    
+                    if digits[current_idx] == digits[idx]:
+                        current_idx += 1
+                        continue
                 
-                
+                if current_idx == total_digits - 1:
+                    return int("".join(digits))
