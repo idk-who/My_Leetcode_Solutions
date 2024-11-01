5cf1f20581315ddf0f16a1725532f411200738a3
idk-who <idontknowwhodoi@gmail.com>
2024-11-01 23:59:38 +0530

    Time: 382 ms (20.72%) | Memory: 18.2 MB (88.45%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        ans = s[:2]
+
+        for i in range(2, len(s)):
+            if s[i] == s[i-1] == s[i-2]:
+                continue
+            else:
+                ans += s[i]
+        
+        return ans
