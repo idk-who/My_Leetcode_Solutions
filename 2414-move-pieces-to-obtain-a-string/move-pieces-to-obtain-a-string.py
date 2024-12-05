e4c4d2e0cde800513ad3dc96b9f3b4f5002596ef
idk-who <idontknowwhodoi@gmail.com>
2024-12-05 23:42:57 +0530

    Time: 281 ms (12.90%) | Memory: 18.1 MB (36.53%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        n = len(start)
+        p1 = 0
+        p2 = 0
+
+        while p1 < n or p2 < n:
+            while p1 < n and start[p1] == "_" :
+                p1 += 1
+            while p2 < n and target[p2] == "_":
+                p2 += 1
+            print(p1, p2)
+            if p1 == n and p2 == n:
+                return True
+            if p1 == n or p2 == n:
+                return False
+            if start[p1] == target[p2]:
+                if start[p1] == 'L':
+                    if p1 < p2:
+                        return False
+                else:
+                    if p1 > p2:
+                        return False
+                p1 += 1
+                p2 += 1
+            else:
+                return False
+
+        return True
