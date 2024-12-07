9d3266037306c60e5d113f9193b4d7d4965de1c8
idk-who <idontknowwhodoi@gmail.com>
2024-12-08 02:20:38 +0530

    Time: 1029 ms (17.00%) | Memory: 29.3 MB (32.16%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+
+        def is_poss(penalty):
+            mop = maxOperations
+            for i in nums:
+                if i > penalty:
+                    mop -= ((i//penalty) + ((i%penalty) > 0)) - 1
+                if mop < 0:
+                    return False
+            return True
+
+        l = 1
+        r = 10**9
+
+        while l < r:
+            m = (l+r)//2
+            if is_poss(m):
+                r = m
+            else:
+                l = m + 1
+        
+        return l
