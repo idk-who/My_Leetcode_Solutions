3bbbf145abc9614cd5ca973404aab8703763f02d
idk-who <idontknowwhodoi@gmail.com>
2024-12-09 23:29:14 +0530

    Time: 171 ms (12.50%) | Memory: 47.4 MB (23.08%) - LeetSync

--- 3427-special-array-ii/special-array-ii.py
+++ 3427-special-array-ii/special-array-ii.py
@@ -9,8 +9,8 @@ class Solution:
         # print(starts)
         ans = []
         for s, e in queries:
-            p1 = bisect.bisect(starts, s)
-            p2 = bisect.bisect(starts, e)
-            # print(p1, p2)
+            p1 = bisect.bisect_right(starts, s)
+            p2 = bisect.bisect_right(starts, e)
+            print(p1, p2)
             ans.append(p1==p2)
         return ans
