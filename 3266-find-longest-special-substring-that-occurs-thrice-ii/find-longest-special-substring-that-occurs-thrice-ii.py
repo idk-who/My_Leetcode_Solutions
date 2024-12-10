2c4acc71f4fbd77a1b16a284903a3b0ac09e8211
idk-who <idontknowwhodoi@gmail.com>
2024-12-10 23:34:31 +0530

    Time: 2628 ms (21.66%) | Memory: 102.9 MB (23.46%) - LeetSync

---
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        d = defaultdict(int)
+
+        d[(s[0], 1)] += 1
+        le = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                le += 1
+                d[(s[i], le)] += 1
+                if le > 1:
+                    d[(s[i], le-1)] += 1
+                    if le > 2:
+                        d[(s[i], le-2)] += 1
+            else:
+                le = 1
+                d[(s[i], le)] += 1
+            # print(d.items())
+        ans = -1
+        
+        for (c, le), k in d.items():
+            # print(c, le, k)
+            if k >= 3:
+                ans = max(ans, le)
+        
+        return ans
