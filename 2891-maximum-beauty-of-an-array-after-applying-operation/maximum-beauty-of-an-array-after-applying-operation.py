8f2ead8cd605c7b28f3d0749c7c26623748da7f5
idk-who <idontknowwhodoi@gmail.com>
2024-12-11 21:56:20 +0530

    Time: 727 ms (16.23%) | Memory: 28.1 MB (39.52%) - LeetSync

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        ma = 1
+        # print(nums)
+        for i in range(min(nums), max(nums)+1):
+            l = bisect.bisect(nums, i-k)
+            while 0 <= l < len(nums) and nums[l] >= i-k:
+                l -= 1
+            r = bisect.bisect(nums, i+k) - 1
+            # print(i, l, r)
+            ma = max(ma, r-l)
+        
+        return ma
