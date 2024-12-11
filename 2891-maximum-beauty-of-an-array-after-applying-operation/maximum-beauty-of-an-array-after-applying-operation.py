781a464ae3ebdc64cfc83385894434b7d4bef84c
idk-who <idontknowwhodoi@gmail.com>
2024-12-11 22:05:34 +0530

    Time: 2406 ms (5.12%) | Memory: 28.1 MB (34.13%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,38 +2,32 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
 
-        ma = 1
-
         def find_l(nums, n):
             l = 0
             r = len(nums)-1
-
             while l <= r:
                 m = (l+r)//2
                 if nums[m] >= n:
                     r = m - 1
                 else:
                     l = m + 1
-
             return r
 
         def find_r(nums, n):
             l = 0
             r = len(nums)-1
-
             while l <= r:
                 m = (l+r)//2
                 if nums[m] > n:
                     r = m - 1
                 else:
                     l = m + 1
-
             return l
 
+        ma = 1
         for i in range(min(nums), max(nums)+1):
             l = find_l(nums, i-k)
             r = find_r(nums, i+k) - 1
-            # print(i, l, r)
             ma = max(ma, r-l)
         
         return ma
