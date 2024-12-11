5b332c204efbfce32d7312c5e4b772dba7195ddb
idk-who <idontknowwhodoi@gmail.com>
2024-12-11 22:06:25 +0530

    Time: 730 ms (16.23%) | Memory: 28.1 MB (39.52%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,32 +2,15 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
 
-        def find_l(nums, n):
-            l = 0
-            r = len(nums)-1
-            while l <= r:
-                m = (l+r)//2
-                if nums[m] >= n:
-                    r = m - 1
-                else:
-                    l = m + 1
-            return r
-
-        def find_r(nums, n):
-            l = 0
-            r = len(nums)-1
-            while l <= r:
-                m = (l+r)//2
-                if nums[m] > n:
-                    r = m - 1
-                else:
-                    l = m + 1
-            return l
-
         ma = 1
         for i in range(min(nums), max(nums)+1):
-            l = find_l(nums, i-k)
-            r = find_r(nums, i+k) - 1
+            l = bisect.bisect(nums, i-k)
+            while 0 <= l < len(nums) and nums[l] >= i-k:
+                l -= 1
+
+            r = bisect.bisect(nums, i+k) - 1
+
             ma = max(ma, r-l)
         
         return ma
+
