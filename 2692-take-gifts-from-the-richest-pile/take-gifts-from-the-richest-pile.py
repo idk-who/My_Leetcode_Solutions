c255586057f6f95242cb5f8b652c1bba5c93d54f
idk-who <idontknowwhodoi@gmail.com>
2024-12-12 20:04:15 +0530

    Time: 4 ms (62.99%) | Memory: 17.7 MB (8.46%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -1,11 +1,25 @@
 from heapq import heapify, heappop, heappush
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        gifts = [-i for i in gifts]
-        heapify(gifts)
+        ans = 0
+        h = []
+        
+        for g in gifts:
+            if len(h) < k:
+                heappush(h, g)
+            else:
+                if h[0] >= g:
+                    ans += g
+                else:
+                    ans += heappop(h)
+                    heappush(h, g)
+        
+        h = [-i for i in h]
+        heapify(h)
         
         for i in range(k):
-            ele = -heappop(gifts)
-            heappush(gifts, -int(ele**(1/2)))
+            heappush(h, -int((-heappop(h))**(1/2)))
+        ans += -sum(h)
 
-        return -sum(gifts)
+        return ans
+
