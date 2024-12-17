0fe6c9005d2b9fc2fcff93f87be9d5c0c0415bfc
idk-who <idontknowwhodoi@gmail.com>
2024-12-17 23:50:11 +0530

    Time: 268 ms (39.13%) | Memory: 20 MB (5.59%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -1,27 +1,26 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        freq = [0] * 26
-        for char in s:
-            freq[ord(char) - ord("a")] += 1
+        freq = [0]*26
+        for i in s:
+            freq[ord(i)-ord('a')] += 1
+        
+        ans = []
+        ptr = 26-1
 
-        result = []
-        current_char_index = 25  # Start from the largest character
-        while current_char_index >= 0:
-            if freq[current_char_index] == 0:
-                current_char_index -= 1
-                continue
+        while ptr >= 0:
+            to_use = min(freq[ptr], repeatLimit)
+            ans += [chr(ptr+ord('a'))]*to_use
+            freq[ptr] -= to_use
 
-            use = min(freq[current_char_index], repeatLimit)
-            result.append(chr(current_char_index + ord("a")) * use)
-            freq[current_char_index] -= use
-
-            if freq[current_char_index] > 0:  # Need to add a smaller character
-                smaller_char_index = current_char_index - 1
-                while smaller_char_index >= 0 and freq[smaller_char_index] == 0:
-                    smaller_char_index -= 1
-                if smaller_char_index < 0:
+            if freq[ptr] > 0:
+                next_chr = ptr - 1
+                while next_chr >= 0 and freq[next_chr] == 0:
+                    next_chr -= 1
+                if next_chr < 0:
                     break
-                result.append(chr(smaller_char_index + ord("a")))
-                freq[smaller_char_index] -= 1
-
-        return "".join(result)
+                ans += [chr(next_chr+ord('a'))]
+                freq[next_chr] -= 1
+            else:
+                ptr -= 1
+        
+        return "".join(ans)
