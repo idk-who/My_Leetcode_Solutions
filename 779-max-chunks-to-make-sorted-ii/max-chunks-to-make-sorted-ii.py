357beef2666d6b4c9eea2197b2e77edbba30d065
idk-who <idontknowwhodoi@gmail.com>
2024-12-19 23:10:18 +0530

    Time: 11 ms (27.51%) | Memory: 18.3 MB (5.58%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(arr)
 
         prefixMax = [0]*len(arr)
-        ma = 0
+        ma = float('-inf')
         for i in range(n):
             ma = max(arr[i], ma)
             prefixMax[i] = ma
@@ -13,7 +13,7 @@ class Solution:
         for i in range(n-1, -1, -1):
             mi = min(arr[i], mi)
             suffixMin[i] = mi
-            
+
         chunks = 1
         for i in range(1, n):
             if suffixMin[i] >= prefixMax[i-1]:
