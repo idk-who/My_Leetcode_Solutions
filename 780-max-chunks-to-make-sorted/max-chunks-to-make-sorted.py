9b6bd43bd16b9d6c7ba916e94d7cd2d3c36b2b91
idk-who <idontknowwhodoi@gmail.com>
2024-12-19 22:20:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (14.45%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -1,14 +1,28 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         present = [0]*len(arr)
-
         ans = 0
         prev = 0
+        ts = 0  #total sum
+        si = 0  #sum from prev till i
+
         for i in range(len(arr)):
             present[arr[i]] = 1
-            # print(present)
-            if sum(present[prev:i+1]) == sum(present[prev:]) == i - prev + 1:
+
+            # total sum
+            ts += 1
+            
+            # collecting from past occurunces
+            si += present[i]
+            # collecting if past occurs
+            if prev <= arr[i] < i:
+                si += 1
+                
+            # print(ts, si)
+            if si == ts == i - prev + 1:
                 ans += 1
                 prev = i + 1
+                ts = ts - si
+                si = 0
         
         return ans
