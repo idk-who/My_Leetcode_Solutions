91f9d876882d4830de0b2ac1c09d58eb98b27b55
idk-who <idontknowwhodoi@gmail.com>
2024-12-19 23:10:06 +0530

    Time: 9 ms (30.86%) | Memory: 18.3 MB (5.58%) - LeetSync

---
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n = len(arr)
+
+        prefixMax = [0]*len(arr)
+        ma = 0
+        for i in range(n):
+            ma = max(arr[i], ma)
+            prefixMax[i] = ma
+
+        suffixMin = [0]*len(arr)
+        mi = float('inf')
+        for i in range(n-1, -1, -1):
+            mi = min(arr[i], mi)
+            suffixMin[i] = mi
+            
+        chunks = 1
+        for i in range(1, n):
+            if suffixMin[i] >= prefixMax[i-1]:
+                chunks += 1
+        
+        return chunks
