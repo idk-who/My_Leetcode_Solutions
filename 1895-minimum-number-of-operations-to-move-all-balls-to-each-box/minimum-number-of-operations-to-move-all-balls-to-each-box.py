23d89ef3fce1a22c5da09314145d1c9999096227
idk-who <idontknowwhodoi@gmail.com>
2025-01-07 00:13:13 +0530

    Time: 11 ms (68.21%) | Memory: 18 MB (33.80%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,19 +1,20 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        left = [0]*n
-        right = [0]*n
+        balls_left = 0
+        moves_left = 0 
+        balls_right = 0
+        moves_right = 0
 
-        for i in range(1, n):
-            left[i] = left[i-1] + (boxes[i-1] == '1')
+        ans = [0]*n
+        for i in range(n):
+            moves_left += balls_left
+            balls_left += (boxes[i] == '1')
+            ans[i] += moves_left
         
-        for i in range(n-2, -1, -1):
-            right[i] = right[i+1] + (boxes[i+1] == '1')
+        for i in range(n-1, -1, -1):
+            moves_right += balls_right
+            balls_right += (boxes[i] == '1')
+            ans[i] += moves_right
         
-        for i in range(1, n):
-            left[i] += left[i-1]
-        
-        for i in range(n-2, -1, -1):
-            right[i] += right[i+1]
-        
-        return [i+j for i, j in zip(left, right)]
+        return ans
