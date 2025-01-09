cb8cf984c2a4cc9ff330f8c7e42f0867bae54f13
idk-who <idontknowwhodoi@gmail.com>
2025-01-09 23:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (27.56%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        ans = 0
+
+        for w in words:
+            if w.startswith(pref):
+                ans += 1
+        
+        return ans
