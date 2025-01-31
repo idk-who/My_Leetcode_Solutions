f5763fd8abaa3cfb24da87de4e2503723bd47fbd
idk-who <idontknowwhodoi@gmail.com>
2025-02-01 00:04:13 +0530

    Time: 478 ms (97.87%) | Memory: 27.2 MB (90.95%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -1,112 +1,59 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        island_sizes = {}
+        n = len(grid)
+
+        def calc_size(grid, i, j, island_id):
+            if 0<=i<n and 0<=j<n and grid[i][j] == 1:
+                grid[i][j] = island_id
+                return (
+                    1 +
+                    calc_size(grid, i-1, j, island_id) +
+                    calc_size(grid, i+1, j, island_id) +
+                    calc_size(grid, i, j-1, island_id) +
+                    calc_size(grid, i, j+1, island_id)
+                )
+            return 0
+        
+        island_sizes = dict()
         island_id = 2
 
-        # Step 1: Mark all islands and calculate their sizes
-        for current_row in range(len(grid)):
-            for current_column in range(len(grid[0])):
-                if grid[current_row][current_column] == 1:
-                    island_sizes[island_id] = self.explore_island(
-                        grid, island_id, current_row, current_column
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    island_sizes[island_id] = calc_size(
+                        grid, i, j, island_id
                     )
                     island_id += 1
 
-        # If there are no islands, return 1
-        if not island_sizes:
+        if len(island_sizes) == 0:
             return 1
-
-        # If the entire grid is one island, return its size or size +
+        
         if len(island_sizes) == 1:
-            island_id -= 1
-            return (
-                island_sizes[island_id]
-                if island_sizes[island_id] == len(grid) * len(grid[0])
-                else island_sizes[island_id] + 1
-            )
+            return min(island_sizes[island_id-1]+1, n*n)
 
-        max_island_size = 1
 
-        # Step 2: Try converting every 0 to 1 and calculate the resulting island size
-        for current_row in range(len(grid)):
-            for current_column in range(len(grid[0])):
-                if grid[current_row][current_column] == 0:
-                    current_island_size = 1
-                    neighboring_islands = set()
+        def get_neighbours(i, j):
+            neighbours = set()
+            for _i, _j in [[i-1, j], [i+1, j], [i, j-1], [i, j+1]]:
+                if 0<=_i<n and 0<=_j<n and grid[_i][_j] > 0:
+                    neighbours.add(grid[_i][_j])
+            return neighbours
 
-                    # Check down
-                    if (
-                        current_row + 1 < len(grid)
-                        and grid[current_row + 1][current_column] > 1
-                    ):
-                        neighboring_islands.add(
-                            grid[current_row + 1][current_column]
-                        )
+        max_size = 1
 
-                    # Check up
-                    if (
-                        current_row - 1 >= 0
-                        and grid[current_row - 1][current_column] > 1
-                    ):
-                        neighboring_islands.add(
-                            grid[current_row - 1][current_column]
-                        )
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    curr_size = 1
 
-                    # Check right
-                    if (
-                        current_column + 1 < len(grid[0])
-                        and grid[current_row][current_column + 1] > 1
-                    ):
-                        neighboring_islands.add(
-                            grid[current_row][current_column + 1]
-                        )
+                    neighbours = get_neighbours(i, j)
+                    
+                    for island_id in neighbours:
+                        curr_size += island_sizes[island_id]
+                    
+                    max_size = max(max_size, curr_size)
+                    
+        return max_size
 
-                    # Check left
-                    if (
-                        current_column - 1 >= 0
-                        and grid[current_row][current_column - 1] > 1
-                    ):
-                        neighboring_islands.add(
-                            grid[current_row][current_column - 1]
-                        )
 
-                    # Sum the sizes of all unique neighboring islands
-                    for island_id in neighboring_islands:
-                        current_island_size += island_sizes[island_id]
-                    max_island_size = max(max_island_size, current_island_size)
-
-        return max_island_size
-
-    def explore_island(
-        self,
-        grid: List[List[int]],
-        island_id: int,
-        current_row: int,
-        current_column: int,
-    ) -> int:
-        if (
-            current_row < 0
-            or current_row >= len(grid)
-            or current_column < 0
-            or current_column >= len(grid[0])
-            or grid[current_row][current_column] != 1
-        ):
-            return 0
-
-        grid[current_row][current_column] = island_id
-
-        return (
-            1
-            + self.explore_island(
-                grid, island_id, current_row + 1, current_column
-            )
-            + self.explore_island(
-                grid, island_id, current_row - 1, current_column
-            )
-            + self.explore_island(
-                grid, island_id, current_row, current_column + 1
-            )
-            + self.explore_island(
-                grid, island_id, current_row, current_column - 1
-            )
-        )
+        
