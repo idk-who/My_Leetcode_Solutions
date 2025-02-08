3199af9396990ed5b8da1a635739721956d583c8
idk-who <idontknowwhodoi@gmail.com>
2025-02-08 20:03:41 +0530

    Time: 75 ms (98.04%) | Memory: 76.6 MB (75.98%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -2,30 +2,22 @@ from heapq import heappush, heappop
 class NumberContainers:
 
     def __init__(self):
-        self.nu_in = defaultdict(list)
         self.in_nu = dict()
-        self.nu_removed = defaultdict(set)
+        self.nu_in = defaultdict(list)
 
     def change(self, index: int, number: int) -> None:
-        if index in self.in_nu:
-            ele = self.in_nu[index]
-            self.nu_removed[ele].add(index)
         self.in_nu[index] = number
-        if index in self.nu_removed[number]:
-            self.nu_removed[number].remove(index)
         heappush(self.nu_in[number], index)
 
     def find(self, number: int) -> int:
-        ind = -1
         while self.nu_in[number]:
-            temp = self.nu_in[number][0]
-            if temp in self.nu_removed[number]:
+            ind = self.nu_in[number][0]
+            if self.in_nu[ind] != number:
                 heappop(self.nu_in[number])
             else:
-                ind = temp
-                break
+                return ind
         
-        return ind
+        return -1
         
 
 
