78339f1670d4b75ffac66840926e5a677260862d
idk-who <idontknowwhodoi@gmail.com>
2025-02-12 22:25:45 +0530

    Time: 19 ms (14.98%) | Memory: 17.8 MB (76.11%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -1,12 +1,14 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        # Continue removing occurrences of 'part' as long as it exists in 's'
-        while part in s:
-            # Find the index of the leftmost occurrence of 'part'
-            part_start_index = s.find(part)
+        stack = []
+        n, m = len(s), len(part)
 
-            # Remove the substring 'part' by concatenating segments before and after 'part'
-            s = s[:part_start_index] + s[part_start_index + len(part) :]
+        for i in range(n):
+            stack.append(s[i])
 
-        # Return the updated string after all occurrences are removed
-        return s
+            while len(stack) >= m and "".join(stack[len(stack)-m:]) == part:
+                    stack = stack[:-m]
+        return "".join(stack)
+        
+
+        
