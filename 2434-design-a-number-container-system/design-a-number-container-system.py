b828cf169ee766f3265f7964fa1b194a564f5836
idk-who <idontknowwhodoi@gmail.com>
2025-02-17 18:10:26 +0530

    Time: 716 ms (6.67%) | Memory: 151.7 MB (21.93%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -1,29 +1,22 @@
+from sortedcontainers import SortedSet
 class NumberContainers:
 
     def __init__(self):
-        # Initializing the defaultdict with SortedSet and the regular dictionary
-        # Map from number to set of indices
-        self.number_to_indices = collections.defaultdict(SortedSet)
-        # Map from index to number
-        self.index_to_number = {}
+        self.in_nu = dict()
+        self.nu_in = defaultdict(SortedSet)
 
     def change(self, index: int, number: int) -> None:
-        # If index already has a number, remove it from the old number's index set
-        if index in self.index_to_number:
-            previous_number = self.index_to_number[index]
-            self.number_to_indices[previous_number].remove(index)
-            if not self.number_to_indices[previous_number]:
-                del self.number_to_indices[previous_number]
-
-        # Update the number and add the index to the new number's set
-        self.index_to_number[index] = number
-        self.number_to_indices[number].add(index)
-
+        if index in self.in_nu:
+            prev_nu = self.in_nu[index]
+            self.nu_in[prev_nu].remove(index)
+        self.in_nu[index] = number
+        self.nu_in[number].add(index)
+        
     def find(self, number: int) -> int:
-        # Return the smallest index for the given number, or -1 if not found
-        if number in self.number_to_indices and self.number_to_indices[number]:
-            return self.number_to_indices[number][0]
-        return -1
+        if len(self.nu_in[number]) == 0:
+            return -1
+        return self.nu_in[number][0]
+        
 
 
 # Your NumberContainers object will be instantiated and called as such:
