c4f902469bb9112df5ece12fc7618f95b81a4095
idk-who <idontknowwhodoi@gmail.com>
2025-02-17 18:10:18 +0530

    Time: 687 ms (12.79%) | Memory: 151.7 MB (29.86%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -1,24 +1,29 @@
-from heapq import heappush, heappop
 class NumberContainers:
 
     def __init__(self):
-        self.in_nu = dict()
-        self.nu_in = defaultdict(list)
+        # Initializing the defaultdict with SortedSet and the regular dictionary
+        # Map from number to set of indices
+        self.number_to_indices = collections.defaultdict(SortedSet)
+        # Map from index to number
+        self.index_to_number = {}
 
     def change(self, index: int, number: int) -> None:
-        self.in_nu[index] = number
-        heappush(self.nu_in[number], index)
+        # If index already has a number, remove it from the old number's index set
+        if index in self.index_to_number:
+            previous_number = self.index_to_number[index]
+            self.number_to_indices[previous_number].remove(index)
+            if not self.number_to_indices[previous_number]:
+                del self.number_to_indices[previous_number]
+
+        # Update the number and add the index to the new number's set
+        self.index_to_number[index] = number
+        self.number_to_indices[number].add(index)
 
     def find(self, number: int) -> int:
-        while self.nu_in[number]:
-            ind = self.nu_in[number][0]
-            if self.in_nu[ind] != number:
-                heappop(self.nu_in[number])
-            else:
-                return ind
-        
+        # Return the smallest index for the given number, or -1 if not found
+        if number in self.number_to_indices and self.number_to_indices[number]:
+            return self.number_to_indices[number][0]
         return -1
-        
 
 
 # Your NumberContainers object will be instantiated and called as such:
