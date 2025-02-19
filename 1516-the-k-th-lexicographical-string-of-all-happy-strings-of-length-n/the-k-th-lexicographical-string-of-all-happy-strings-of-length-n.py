e262ba0ad62ea4292966198a736431b814f5a17c
idk-who <idontknowwhodoi@gmail.com>
2025-02-19 21:13:05 +0530

    Time: 8 ms (59.74%) | Memory: 17.8 MB (66.08%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,34 +1,21 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        current_string = ""
-        happy_strings = []
-        # Generate all happy strings of length n
-        self.generate_happy_strings(n, current_string, happy_strings)
-
-        # Check if there are at least k happy strings
-        if len(happy_strings) < k:
-            return ""
-
-        # Sort the happy strings in lexicographical order
-        happy_strings.sort()
-
-        return happy_strings[k - 1]
-
-    def generate_happy_strings(
-        self, n: int, current_string: str, happy_strings: list
-    ):
-        # If the current string has reached the desired length, add it to the list
-        if len(current_string) == n:
-            happy_strings.append(current_string)
-            return
-
-        # Try adding each character ('a', 'b', 'c') to the current string
-        for current_char in ["a", "b", "c"]:
-            # Skip if the current character is the same as the last character
-            if len(current_string) > 0 and current_string[-1] == current_char:
-                continue
-
-            # Recursively generate the next character
-            self.generate_happy_strings(
-                n, current_string + current_char, happy_strings
-            )
+        alphas = ['a', 'b', 'c']
+        def gen(n, i, prev, string):
+            if i == n:
+                yield "".join(string)
+                return
+            for w in alphas:
+                if w != prev:
+                    string.append(w)
+                    yield from gen(n, i+1, w, string)
+                    string.pop()
+        
+        ptr = 1
+        
+        for s in gen(n, 0, '', []):
+            if ptr == k:
+                return str(s)
+            ptr += 1
+        
+        return ""
