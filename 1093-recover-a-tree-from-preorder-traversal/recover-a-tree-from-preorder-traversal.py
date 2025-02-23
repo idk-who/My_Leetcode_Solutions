88520a3684c539c8610204b32b92fb2668be989b
idk-who <idontknowwhodoi@gmail.com>
2025-02-23 21:40:05 +0530

    Time: 27 ms (23.46%) | Memory: 18.2 MB (80.06%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
@@ -5,40 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.index = 0
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        depth = []
+        nodes = []
+        dashes = 0
 
-    def recoverFromPreorder(self, traversal: str) -> TreeNode:
-        return self.helper(traversal, 0)
-
-    def helper(self, traversal, depth):
-        if self.index >= len(traversal):
-            return None
-
-        dash_count = 0
-        while (
-            self.index + dash_count < len(traversal)
-            and traversal[self.index + dash_count] == "-"
-        ):
-            dash_count += 1
-
-        # If the number of dashes doesn't match the current depth, return null
-        if dash_count != depth:
-            return None
-
-        self.index += dash_count
-
-        # Extract the node value
-        value = 0
-        while self.index < len(traversal) and traversal[self.index].isdigit():
-            value = value * 10 + int(traversal[self.index])
-            self.index += 1
-
-        # Create the current node
-        node = TreeNode(value)
-
-        # Recursively build the left and right subtrees
-        node.left = self.helper(traversal, depth + 1)
-        node.right = self.helper(traversal, depth + 1)
-
-        return node    
+        ptr = 0 
+        while ptr < len(traversal):
+            if traversal[ptr] == '-':
+                dashes += 1
+                ptr += 1
+            else:
+                num = 0
+                while ptr < len(traversal) and traversal[ptr] != '-':
+                    num = num*10 + int(traversal[ptr])
+                    ptr += 1
+                depth.append(dashes)
+                nodes.append(num)
+                dashes = 0
+        
+        self.ptr = 0
+        def rec(nodes, curr_depth):
+            if self.ptr < len(nodes) and depth[self.ptr] == curr_depth:
+                root = TreeNode(nodes[self.ptr])
+                self.ptr += 1
+                root.left = rec(nodes, curr_depth+1)
+                root.right = rec(nodes, curr_depth+1)
+                return root
+            
+        return rec(nodes, 0)
+        
