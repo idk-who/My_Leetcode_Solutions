84625054d5c60b45882a5dc50f5a728e325aeefe
idk-who <idontknowwhodoi@gmail.com>
2025-03-03 21:53:21 +0530

    Time: 78 ms (8.80%) | Memory: 33.4 MB (97.71%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,5 +1,5 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         nums.sort(key = lambda x: x > pivot)
-        nums.sort(key = lambda x: x >= pivot)
+        nums.sort(key = lambda x: x < pivot, reverse=True)
         return nums
