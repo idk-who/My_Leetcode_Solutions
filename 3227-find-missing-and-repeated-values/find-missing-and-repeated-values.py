2922c65918ee99f22c60d3626d8dfbd12f0f3551
idk-who <idontknowwhodoi@gmail.com>
2025-03-07 13:29:43 +0530

    Time: 13 ms (35.14%) | Memory: 18 MB (97.40%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,18 +1,29 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
-        freq = {}
 
-        # Store frequency of each number in the grid
-        for row in grid:
-            for num in row:
-                freq[num] = freq.get(num, 0) + 1
+        xor = 0
+        for i in range(1, n**2 + 1):
+            xor ^= i
+        for i in grid:
+            for j in i:
+                xor ^= j
 
-        # Check numbers from 1 to n^2 to find missing and repeated values
-        for num in range(1, n * n + 1):
-            if num not in freq:
-                missing = num  # Number not present in grid
-            elif freq[num] == 2:
-                repeat = num  # Number appears twice
-
-        return [repeat, missing]
+        one, two = 0, 0
+        for i in range(1, n**2 + 1):
+            if i & (xor&~(xor-1)):
+                one ^= i
+            else:
+                two ^= i
+        for i in grid:
+            for j in i:
+                if j & (xor&~(xor-1)):
+                    one ^= j
+                else:
+                    two ^= j
+            
+        for i in grid:
+            if one in i:
+                return [one, two]
+        
+        return [two, one]
