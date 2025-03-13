ea2402567a8022a112d92e34cedad076482a0f23
idk-who <idontknowwhodoi@gmail.com>
2025-03-13 23:16:21 +0530

    Time: 1069 ms (35.28%) | Memory: 64.3 MB (23.15%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        n = len(nums)
+        def is_zero(k):
+            pre = [0]*n
+            for i in range(k+1):
+                l, r, v = queries[i]
+                pre[l] += v
+                if r+1 < n:
+                    pre[r+1] -= v
+            su = 0
+            for i in range(n):
+                su += pre[i]
+                if su < nums[i]:
+                    return False
+            return True
+
+        if sum(nums) == 0: return 0
+        # for i in range(len(queries)):
+        #     if is_zero(i):
+        #         return i + 1
+        
+        ind = bisect.bisect_left(
+            list(range(len(queries))), 
+            1, 
+            key = lambda x: is_zero(x)
+        )
+        if ind+1 <= len(queries):
+            return ind+1
+        return -1
+        
