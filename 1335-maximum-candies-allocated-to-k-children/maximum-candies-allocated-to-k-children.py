0740760de09d3c7309cad66f2d71382d29766c50
idk-who <idontknowwhodoi@gmail.com>
2025-03-14 23:43:39 +0530

    Time: 709 ms (8.58%) | Memory: 29.9 MB (18.92%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        if sum(candies) < k:
+            return 0
+        n = len(candies)
+        def can_allocate(num):
+            children = k
+            ptr = 0
+            while children > 0 and ptr < n:
+                children -= candies[ptr]//num
+                ptr += 1
+            
+            return children <= 0
+        
+        mi = 1
+        ma = max(candies)
+
+        while mi <= ma:
+            mid = (mi+ma)//2
+            if can_allocate(mid):
+                mi = mid + 1
+            else:
+                ma = mid - 1
+        
+        return ma
