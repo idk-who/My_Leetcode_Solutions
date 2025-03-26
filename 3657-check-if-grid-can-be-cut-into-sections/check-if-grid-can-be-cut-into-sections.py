73d908129eeda1fbe6bef58fc0635c7d2752904e
idk-who <idontknowwhodoi@gmail.com>
2025-03-26 10:58:48 +0530

    Time: 288 ms (84.72%) | Memory: 83.5 MB (70.14%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -1,22 +1,22 @@
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
         rectangles.sort()
-
         vertical = 0
         ma_end = rectangles[0][2]
         for s, _, e, _ in rectangles:
-            # print(ma_end, s, e)
             if ma_end <= s:
                 vertical += 1
             ma_end = max(ma_end, e)
         
+        if vertical >= 2:
+            return True
+
         rectangles.sort(key = lambda x: x[1])
         horizontal = 0
         ma_end = rectangles[0][3]
         for _, s, _, e in rectangles:
-            # print(ma_end, s, e)
             if ma_end <= s:
                 horizontal += 1
             ma_end = max(ma_end, e)
-        print(vertical, horizontal)
-        return vertical >= 2 or horizontal >= 2
+
+        return horizontal >= 2
