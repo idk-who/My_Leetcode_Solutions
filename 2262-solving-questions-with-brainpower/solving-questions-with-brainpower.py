813097fb322d036c16a3e74c526ac9fcd6505d1e
idk-who <idontknowwhodoi@gmail.com>
2025-04-01 23:10:42 +0530

    Time: 154 ms (39.16%) | Memory: 66.5 MB (37.11%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -0,0 +1,20 @@
+from collections import deque
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = dict()
+        def rec(ptr):
+            if ptr >= n:
+                return 0
+            if ptr in dp:
+                return dp[ptr]
+            
+            ma = max(
+                rec(ptr+1),
+                questions[ptr][0] + rec(ptr+1+questions[ptr][1])
+            )
+
+            dp[ptr] = ma
+            return ma
+        
+        return rec(0)
