5938570554dfb8cf31a59cb4ba26b313468d3179
idk-who <idontknowwhodoi@gmail.com>
2025-04-04 00:56:08 +0530

    Time: 6 ms (23.85%) | Memory: 18.6 MB (38.99%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,24 +1,33 @@
 class Solution:
-    def setZeroes(self, grid: List[List[int]]) -> None:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n, m = len(grid), len(grid[0])
-        col_zero = 1
-
+        n, m = len(matrix), len(matrix[0])
+        first_row = False
+        first_col = False
         for i in range(n):
-            if grid[i][0] == 0: col_zero = 0
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    if i == 0:
+                        first_row = True
+                    else:
+                        matrix[i][0] = 0
+                    if j == 0:
+                        first_col = True
+                    else:
+                        matrix[0][j] = 0
+        
+        for i in range(1, n):
             for j in range(1, m):
-                if grid[i][j] == 0:
-                    grid[i][0] = 0
-                    grid[0][j] = 0
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
 
-        for i in range(n-1, -1, -1):
-            for j in range(m-1, 0, -1):
-                if grid[i][0] == 0 or grid[0][j] == 0:
-                    grid[i][j] = 0
-            if col_zero == 0: grid[i][0] = 0
+        if first_col:
+            for i in range(n):
+                matrix[i][0] = 0
         
-        
-
+        if first_row:
+            for j in range(m):
+                matrix[0][j] = 0
         
