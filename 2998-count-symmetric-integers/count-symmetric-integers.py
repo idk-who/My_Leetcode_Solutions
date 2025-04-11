73ca8469535d30ceea5ef82bb0681d892a718124
idk-who <idontknowwhodoi@gmail.com>
2025-04-12 00:12:06 +0530

    Time: 148 ms (95.32%) | Memory: 17.7 MB (96.13%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.py
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -3,8 +3,11 @@ class Solution:
         ans = 0
         for i in range(low, high+1):
             if not (len(str(i))&1):
-                le = len(str(i))
-                if sum([int(j) for j in str(i)[:le//2]]) == sum([int(j) for j in str(i)[le//2:]]):
-                    ans += 1
-        
+                if i < 100:
+                    if i//10 == i%10:
+                        ans += 1
+                else:
+                    if i // 1000 + (i % 1000 // 100) == i % 100 // 10 + i % 10:
+                        ans += 1
+                
         return ans
