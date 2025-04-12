31df356f43c0bd0f509afbec47f7567944cefeeb
idk-who <idontknowwhodoi@gmail.com>
2025-04-12 23:45:39 +0530

    Time: 873 ms (91.94%) | Memory: 18.9 MB (96.77%) - LeetSync

--- 3548-find-the-count-of-good-integers/find-the-count-of-good-integers.py
+++ 3548-find-the-count-of-good-integers/find-the-count-of-good-integers.py
@@ -1,25 +1,28 @@
 class Solution:
     def countGoodIntegers(self, n: int, k: int) -> int:
-        is_odd = (n&1)
-        def creat_num(i):
-            num = str(i)
-            num += str(i)[::-1][is_odd:]
-            return num
+        dictionary = set()
+        base = 10 ** ((n - 1) // 2)
+        skip = n & 1
+        # Enumerate the number of palindrome numbers of n digits
+        for i in range(base, base * 10):
+            s = str(i)
+            s += s[::-1][skip:]
+            palindromicInteger = int(s)
+            # If the current palindrome number is a k-palindromic integer
+            if palindromicInteger % k == 0:
+                sorted_s = "".join(sorted(s))
+                dictionary.add(sorted_s)
 
-        s = set()
-        le = n//2+(n&1)
-        for i in range(10**(le-1), 10**le):
-            num = creat_num(i)
-            if int(num) % k == 0:
-                s.add("".join(sorted(num)))
-
+        fac = [factorial(i) for i in range(n + 1)]
         ans = 0
-        for i in s:
-            cnt = Counter(i)
-            perms = (n-cnt['0']) * factorial(n-1)
-            for _, j in cnt.items():
-                perms //= factorial(j)
-            ans += perms
+        for s in dictionary:
+            cnt = [0] * 10
+            for c in s:
+                cnt[int(c)] += 1
+            # Calculate permutations and combinations
+            tot = (n - cnt[0]) * fac[n - 1]
+            for x in cnt:
+                tot //= fac[x]
+            ans += tot
+
         return ans
-        
-        
