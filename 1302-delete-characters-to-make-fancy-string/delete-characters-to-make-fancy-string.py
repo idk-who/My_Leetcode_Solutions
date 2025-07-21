8657602769151a01cf80e33b30965f01f28a3f1f
idk-who <idontknowwhodoi@gmail.com>
2025-07-21 19:00:54 +0530

    Time: 294 ms (82.51%) | Memory: 19.6 MB (52.29%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,11 +1,18 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        ans = s[:2]
 
-        for i in range(2, len(s)):
-            if s[i] == s[i-1] == s[i-2]:
-                continue
+        prev = s[0]
+        frequency = 1
+        ans = s[0]
+
+        for i in range(1, len(s)):
+            if s[i] == prev:
+                frequency += 1
             else:
+                prev = s[i]
+                frequency = 1
+
+            if frequency < 3:
                 ans += s[i]
-        
+
         return ans
