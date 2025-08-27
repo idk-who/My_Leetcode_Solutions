bd2b74d0da66e93063d516c9168f95d31d70b71a
idk-who <idontknowwhodoi@gmail.com>
2025-08-28 01:21:12 +0530

    Time: 6189 ms (5.93%) | Memory: 700.5 MB (19.26%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.py
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.py
@@ -1,21 +1,45 @@
 class Solution:
-    def lenOfVDiagonal(self, grid: List[List[int]]) -> int:        
-        @lru_cache(None)
-        def dfs(row,col, dr,dc, element, hasTurned):
-            if not (0 <= row < m and 0 <= col < n) or grid[row][col] != element:
-                return 0            
-            ans = dfs(row + dr, col + dc, dr, dc, element^2, hasTurned)
-            if hasTurned: return ans + 1
-            length = dfs(row + dc, col - dr, dc, -dr, element^2, True)
-            return max(ans,length) + 1 
-        m, n = len(grid), len(grid[0])
-        ans, dr, dc, hasOne = 0, 1, 1, False 
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 1:
-                    hasOne = True
-                    for _ in range(4):
-                        dr, dc = -dc, dr
-                        vee = dfs(row + dr, col + dc, dr, dc, 2, False)
-                        ans = max(ans, vee)
-        return ans + hasOne
+    def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
+        grid = grid
+        dp = dict()
+        n, m = len(grid), len(grid[0])
+
+        def dfs(i, j, dir, made_turn = False, prev = -1):
+            if 0 <= i < n and 0 <= j < m and grid[i][j] != 1 and grid[i][j] != prev:
+                if (i, j, dir, made_turn) in dp:
+                    return dp[(i, j, dir, made_turn)]
+                else:
+                    org = grid[i][j]
+                    grid[i][j] = 1
+                    new_dir = {
+                        (-1, -1):(-1, 1),
+                        (-1, 1) :(1, 1),
+                        (1, 1)  :(1, -1),
+                        (1, -1) :(-1, -1)
+                    }[dir]
+                    dp[(i, j, dir, made_turn)] = 1 + max(
+                        dfs(i+dir[0], j+dir[1], dir, made_turn, org),
+                        dfs(
+                            i+new_dir[0], j+new_dir[1], new_dir, True, org
+                        ) if not made_turn else 0
+                    )
+                    grid[i][j] = org
+                    return dp[(i, j, dir, made_turn)]
+            else:
+                return 0
+
+        ma = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    ma = max(
+                        ma,
+                        1 + max(
+                            dfs(i-1, j-1, (-1, -1), prev = 0),
+                            dfs(i-1, j+1, (-1, +1), prev = 0),
+                            dfs(i+1, j-1, (+1, -1), prev = 0),
+                            dfs(i+1, j+1, (+1, +1), prev = 0)
+                        )
+                    )
+                    print(i, j, ma)
+        return ma
