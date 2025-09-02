fd203257a1e306de554a7740738daae97727e9fc
idk-who <idontknowwhodoi@gmail.com>
2025-09-03 01:05:39 +0530

    Time: 1836 ms (41.18%) | Memory: 18.3 MB (82.35%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
@@ -1,25 +1,17 @@
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
+        n = len(points)
+        points.sort(key = lambda x: (-x[1], x[0]))
+        # print(points)
         ans = 0
-        points.sort(key=lambda x: (x[0], -x[1]))
-
-        for i in range(len(points) - 1):
-            pointA = points[i]
-            xMin = pointA[0] - 1
-            xMax = math.inf
-            yMin = -math.inf
-            yMax = pointA[1] + 1
-
-            for j in range(i + 1, len(points)):
-                pointB = points[j]
-                if (
-                    pointB[0] > xMin
-                    and pointB[0] < xMax
-                    and pointB[1] > yMin
-                    and pointB[1] < yMax
-                ):
+        for i in range(n):
+            x_ma, y_mi = float('inf'), float('-inf')
+            for j in range(i+1, n):
+                if points[j][0] < points[i][0]:
+                    continue
+                if points[j][0] < x_ma or points[j][1] > y_mi:
                     ans += 1
-                    xMin = pointB[0]
-                    yMin = pointB[1]
-
+                    x_ma = min(x_ma, points[j][0])
+                    y_mi = max(y_mi, points[j][1])
+        
         return ans
