8953d3f883ee969f63c36bff39bd035c773f166a
idk-who <idontknowwhodoi@gmail.com>
2026-01-16 23:18:01 +0530

    Time: 1304 ms (68.29%) | Memory: 53.9 MB (15.85%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.py
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.py
@@ -1,28 +1,18 @@
 class Solution:
-    def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
-        hor = [1]
-        hor.extend(sorted(hFences))
-        hor.append(m)
-        
-        ver = [1]
-        ver.extend(sorted(vFences))
-        ver.append(n)
-        
-        hor_diff = set()
-        for i in range(len(hor)):
-            for j in range(i+1, len(hor)):
-                temp = hor[j]-hor[i]
-                hor_diff.add(temp)
-        
-        ans = -1
-        for i in range(len(ver)):
-            for j in range(i+1, len(ver)):
-                temp = ver[j]-ver[i]
-                if temp > ans and temp in hor_diff:
-                    ans = temp
-        
-        if ans < 0:
-            return -1
-        else:
-            return (ans**2)%(10**9 + 7)
-                
+    def get_edges(self, fences: List[int], border: int) -> set:
+        points = sorted([1] + fences + [border])
+        return {
+            points[j] - points[i]
+            for i in range(len(points))
+            for j in range(i + 1, len(points))
+        }
+
+    def maximizeSquareArea(
+        self, m: int, n: int, hFences: List[int], vFences: List[int]
+    ) -> int:
+        MOD = 10**9 + 7
+        h_edges = self.get_edges(hFences, m)
+        v_edges = self.get_edges(vFences, n)
+
+        max_edge = max(h_edges & v_edges, default=0)
+        return (max_edge * max_edge) % MOD if max_edge else -1
